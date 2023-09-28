5bcaf32b66b21febb46749532188d16007b7c693
Killian Ferrier <killian.ferrier@etu.univ-amu.fr>
2023-09-28 12:02:39 +0200

    Ajout de la structure MVC et du header

---
+++ views/header.php
@@ -0,0 +1,52 @@
+<head>
+    <link rel="stylesheet" href="../styles/header.css">
+    <title>Header</title>
+</head>
+<body>
+    <nav class="navbar navbar-top navbar-expand-lg navbar-light bg-light">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="#">
+                <img src="/img/CyphubLogo.png" alt="Logo Cyphub" width="auto" height="37" class="d-inline-block align-text-top">
+            </a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse justify-content-end" id="navbarSupportedContent">
+                <ul class="navbar-nav responsive-menu ml-auto mb-2 mb-lg-0">
+                    <li class="nav-item">
+                        <form class="d-flex">
+                            <input class="form-control" type="search" placeholder="Search" aria-label="Search">
+                        </form>
+                    </li>
+                    <li class="nav-item">
+                        <a href="#">
+                            <img src="/img/Profil.png" alt="Logo Cyphub" width="37" height="37" class="menu-img">
+                        </a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="#">
+                            <img src="/img/logout.png" alt="Logo Cyphub" width="37" height="37" class="menu-img me-2">
+                        </a>
+                    </li>
+                </ul>
+
+            </div>
+        </div>
+    </nav>
+
+
+    <nav class="navbar navbar-bottom navbar-expand navbar-light bg-light">
+        <div class="container-fluid">
+
+            <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+                <div class="navbar-nav">
+                    <a class="nav-link active" aria-current="page" href="#">Home</a>
+                    <a class="nav-link" href="#">Veille Tech</a>
+                    <a class="nav-link" href="#">Blogs</a>
+                    <a class="nav-link" href="#">Forum</a>
+                </div>
+            </div>
+        </div>
+    </nav>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.0.0-beta1/jquery.js"></script>
+</body>
