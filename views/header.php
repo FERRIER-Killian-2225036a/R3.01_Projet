f5be939fdc1fbec2ee2671fc76b100f02bdc1bd0
Killian Ferrier <killian.ferrier@etu.univ-amu.fr>
2023-10-09 09:28:30 +0200

    Adaptation header au MVC

--- views/header.php
+++ views/header.php
@@ -48,5 +48,4 @@
             </div>
         </div>
     </nav>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.0.0-beta1/jquery.js"></script>
 </body>
---
+++ views/main.php
@@ -0,0 +1,16 @@
+<!doctype html>
+<html lang="fr">
+    <head>
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1"><meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <title>Cyphub</title>
+        <link rel="stylesheet" href="/common_styles/main.css">
+        <link rel="icon" href="/media/public_assets/favicon.png">
+    </head>
+    <body>
+        <?php require "views/header.php";
+        require "common_scripts/general_scripts.php";
+        ?>
+    </body>
+</html>
