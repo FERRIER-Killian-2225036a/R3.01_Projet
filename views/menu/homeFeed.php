8a3ac82cb088fcf83135d7cf35f3eebc01d80572
PierrickCaronc22019290 <krokmou@kali.attack>
2023-10-12 14:07:58 +0200

    Ajout des 3 boutons de redirection sur la page d'accueil

--- views/menu/homeFeed.php
+++ views/menu/homeFeed.php
@@ -36,7 +36,6 @@
 <main>
 
   <section class="py-5 text-left container">
-    <div class="row py-lg-5">
       <div class="col-lg-6 col-md-8 mx-left">
         <h1 class="homeTitle">Bienvenue chez Cyphub !</h1>
         <p class="lead text-body-secondary">Ici est le début de la page home avec juste une phrase qui finira par être modifier </p>
@@ -59,61 +58,52 @@
           </div>
         </p>
       </div>
+      <div class="row py-lg-5">
+          <div class="col-lg- col-md-8 mx-right"
+          <img src="/media/public_assets/homePicture.png" alt="Logo Cyphub" width="37" height="37">
+      </div>
     </div>
   </section>
 </section>
-    
+
+
+
+<section class="py-5 text-center container">
+    <div class="row py-lg-5">
+        <div class="row row-cols-1 row-cols-md-3 mb-3 text-center">
+            <div class="col">
+                <div class="card mb-4 rounded-3 shadow-sm background">
+                    <button type="button" class="w-100 btn btn-lg text-center colorButton">
+                        <img src="../../media/public_assets/logoTech-removebg-preview.png"alt="Logo Cyphub" width="100" height="70">
+                        <p>Veille Technologique</p></button>
+                </div>
+            </div>
+            <div class="col">
+                <div class="card mb-4 rounded-3 shadow-sm background">
+                        <button type="button" class="w-100 btn btn-lg text-center colorButton">
+                        <img src="../../media/public_assets/CyphubLogo.png"alt="Logo Cyphub" width="37" height="37">
+                        <p>Blog</p></button>
+                </div>
+            </div>
+            <div class="col">
+                <div class="card mb-4 rounded-3 shadow-sm background">
+                    <button type="button" class="w-100 btn btn-lg text-center colorButton">
+                        <img src="../../media/public_assets/CyphubLogo.png"alt="Logo Cyphub" width="37" height="37">
+                        <p>Forum</p></button>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
+
+
+
 
   <div class="album py-5 bg-body-tertiary">
     <div class="container">
 
       <div class="row row-cols-1 row-cols-sm-2 row-cols-md-3 g-3">
-        <div class="col">
-          <div class="card shadow-sm">
-            <svg class="bd-placeholder-img card-img-top" width="100%" height="225" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: Thumbnail" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#55595c"></rect><text x="50%" y="50%" fill="#eceeef" dy=".3em">Thumbnail</text></svg>
-            <div class="card-body">
-              <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-              <div class="d-flex justify-content-between align-items-center">
-                <div class="btn-group">
-                  <button type="button" class="btn btn-sm btn-outline-secondary">View</button>
-                  <button type="button" class="btn btn-sm btn-outline-secondary">Edit</button>
-                </div>
-                <small class="text-body-secondary">9 mins</small>
-              </div>
-            </div>
-          </div>
-        </div>
-        <div class="col">
-          <div class="card shadow-sm">
-            <svg class="bd-placeholder-img card-img-top" width="100%" height="225" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: Thumbnail" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#55595c"></rect><text x="50%" y="50%" fill="#eceeef" dy=".3em">Thumbnail</text></svg>
-            <div class="card-body">
-              <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-              <div class="d-flex justify-content-between align-items-center">
-                <div class="btn-group">
-                  <button type="button" class="btn btn-sm btn-outline-secondary">View</button>
-                  <button type="button" class="btn btn-sm btn-outline-secondary">Edit</button>
-                </div>
-                <small class="text-body-secondary">9 mins</small>
-              </div>
-            </div>
-          </div>
-        </div>
-        <div class="col">
-          <div class="card shadow-sm">
-            <svg class="bd-placeholder-img card-img-top" width="100%" height="225" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: Thumbnail" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#55595c"></rect><text x="50%" y="50%" fill="#eceeef" dy=".3em">Thumbnail</text></svg>
-            <div class="card-body">
-              <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-              <div class="d-flex justify-content-between align-items-center">
-                <div class="btn-group">
-                  <button type="button" class="btn btn-sm btn-outline-secondary">View</button>
-                  <button type="button" class="btn btn-sm btn-outline-secondary">Edit</button>
-                </div>
-                <small class="text-body-secondary">9 mins</small>
-              </div>
-            </div>
-          </div>
-        </div>
-
         <div class="col">
           <div class="card shadow-sm">
             <svg class="bd-placeholder-img card-img-top" width="100%" height="225" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: Thumbnail" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#55595c"></rect><text x="50%" y="50%" fill="#eceeef" dy=".3em">Thumbnail</text></svg>
