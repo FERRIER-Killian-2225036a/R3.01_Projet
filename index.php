71b72e770dec3056002d7cb90f30b0293d900ba9
killianFerrierf22012793 <killian.ferrier@etu.univ-amu.fr>
2023-10-12 16:55:50 +0200

    Modification du fichiers index pour intégrer le MVC

--- index.php
+++ index.php
@@ -1,5 +1,41 @@
 <?php
-require "views/main.php";
+// Ce fichier est le point d'entrée de votre application
 
-?>
-<p>hello world</p>
+    require 'core/autoLoad.php';
+    /*
+     url pour notre premier test MVC Hello World,
+     nous n'avons pas d'action précisée on visera celle par défaut
+     /index.php?ctrl=helloworld
+     /helloworld
+     /controleur/nom_action/whatever/whatever2/
+
+    */
+/*
+    $S_controleur = isset($_GET['ctrl']) ? $_GET['ctrl'] : null;
+    $S_action = isset($_GET['action']) ? $_GET['action'] : null;
+
+    Vue::ouvrirTampon(); //  /Noyau/Vue.php : on ouvre le tampon d'affichage, les contrôleurs qui appellent des vues les mettront dedans
+    $O_controleur = new Controleur($S_controleur, $S_action);
+*/
+
+    $S_urlADecortiquer = isset($_GET['url']) ? $_GET['url'] : null;
+    $mapOfPostParameters = isset($_POST) ? $_POST : null;
+
+    motorView::openBuffer(); // on ouvre le tampon d'affichage, les contrôleurs qui appellent des vues les mettront dedans
+
+    try
+    {
+        $O_controleur = new motorController($S_urlADecortiquer, $mapOfPostParameters);
+        $O_controleur->run();
+    }
+    catch (exceptionsController $O_exception)
+    {
+        echo ('Une erreur s\'est produite : ' . $O_exception->getMessage());
+    }
+
+
+    // Les différentes sous-vues ont été "crachées" dans le tampon d'affichage, on les récupère
+    $contenuPourAffichage = motorView::getBufferContent();
+
+    // On affiche le contenu dans la partie body du gabarit général
+    motorView::show('gabarit', array('body' => $contenuPourAffichage));
