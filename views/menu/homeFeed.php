3f9d7c57387aaa1ebbd88745052d83f0fa26619f
Pierrick Caron <pierrick.caron@etu.univ-amu.fr>
2023-10-13 22:17:03 +0200

    Rajout des 3 boutons BLOG, Veille Tech et Forum, sur la page d'accueil. Page d'accueil, bientôt fini.

--- views/menu/homeFeed.php
+++ views/menu/homeFeed.php
@@ -35,20 +35,20 @@
     </div>
 <main>
 
-  <section class="py-5 text-left container">
+  <section class="paddingP1 text-left container">
       <div class="col-lg-6 col-md-8 mx-left">
         <h1 class="homeTitle">Bienvenue chez Cyphub !</h1>
-        <p class="lead text-body-secondary">Ici est le début de la page home avec juste une phrase qui finira par être modifier </p>
+        <p class="colorText lead text-body-secondary">Ici est le début de la page home avec juste une phrase qui finira par être modifier </p>
         <p>
           <div class="col-lg-8 col-md-8 mx-left">
-            <a href="#" class="cta noneColor text-left block">
+            <a href="#" class="cta colorText noneColor text-left block">
               <span class="visual">Se connecter</span>
                 <svg class="svg1" width="13px" height="10px" viewBox="0 0 13 10">
                   <path d="M1,5 L11,5"></path>
                   <polyline points="8 1 12 5 8 9"></polyline>
                 </svg>
             </a>
-            <a href="#" class="cta noneColor text-left block ">
+            <a href="#" class="cta colorText noneColor text-left block ">
               <span class="visual">Créer un compte</span>
                 <svg class="svg1" width="13px" height="10px" viewBox="0 0 13 10">
                   <path d="M1,5 L11,5"></path>
@@ -58,95 +58,37 @@
           </div>
         </p>
       </div>
-      <div class="row py-lg-5">
-          <div class="col-lg- col-md-8 mx-right"
-          <img src="/media/public_assets/homePicture.png" alt="Logo Cyphub" width="37" height="37">
-      </div>
+      <img src="/media/public_assets/homePicture.png" alt="Logo Cyphub" width="37" height="37">
     </div>
   </section>
 </section>
 
 
 
-<section class="py-5 text-center container">
-    <div class="row py-lg-5">
+<section class="py-3 text-center container ">
+    <div class="py-lg-5">
         <div class="row row-cols-1 row-cols-md-3 mb-3 text-center">
             <div class="col">
-                <div class="card mb-4 rounded-3 shadow-sm background">
-                    <button type="button" class="w-100 btn btn-lg text-center colorButton">
-                        <img src="../../media/public_assets/logoTech-removebg-preview.png"alt="Logo Cyphub" width="100" height="70">
-                        <p>Veille Technologique</p></button>
+                <div class="card mb-4 rounded-4 shadow-gn background">
+                    <a href="technologyWatchFeed.php" type="button" class="w-20 rounded-4 btn btn-lg text-center colorButton shadow-gn grow-button">
+                        <img class="sizeRs" src="../../media/public_assets/iconeTech.png"alt="Icone Veille Tehcnologique " height="150" width="150">
+                        <p class="colorText">Veille Technologique</p></a>
                 </div>
             </div>
             <div class="col">
-                <div class="card mb-4 rounded-3 shadow-sm background">
-                        <button type="button" class="w-100 btn btn-lg text-center colorButton">
-                        <img src="../../media/public_assets/CyphubLogo.png"alt="Logo Cyphub" width="37" height="37">
-                        <p>Blog</p></button>
+                <div class="card mb-4 rounded-4 shadow-gn background">
+                    <a href="blogFeed.php" type="button" class="w-20 rounded-4 btn btn-lg text-center colorButton shadow-gn grow-button">
+                        <img class="sizeRs" src="../../media/public_assets/iconeBlog.png"alt="Icone Blog" height="150" width="150">
+                        <p class="colorText">Blog</p></a>
                 </div>
             </div>
             <div class="col">
-                <div class="card mb-4 rounded-3 shadow-sm background">
-                    <button type="button" class="w-100 btn btn-lg text-center colorButton">
-                        <img src="../../media/public_assets/CyphubLogo.png"alt="Logo Cyphub" width="37" height="37">
-                        <p>Forum</p></button>
+                <div class="card mb-4 rounded-4 shadow-gn background">
+                    <a href="forumFeed.php" type="button" class="w-20 rounded-4 btn btn-lg text-center colorButton shadow-gn grow-button">
+                        <img class="sizeRs" src="../../media/public_assets/iconeForum.png"alt="Icone Forum" height="150" width="150">
+                        <p class="colorText">Forum</p></a>
                 </div>
             </div>
         </div>
     </div>
 </section>
-
-
-
-
-
-  <div class="album py-5 bg-body-tertiary">
-    <div class="container">
-
-      <div class="row row-cols-1 row-cols-sm-2 row-cols-md-3 g-3">
-        <div class="col">
-          <div class="card shadow-sm">
-            <svg class="bd-placeholder-img card-img-top" width="100%" height="225" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: Thumbnail" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#55595c"></rect><text x="50%" y="50%" fill="#eceeef" dy=".3em">Thumbnail</text></svg>
-            <div class="card-body">
-              <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-              <div class="d-flex justify-content-between align-items-center">
-                <div class="btn-group">
-                  <button type="button" class="btn btn-sm btn-outline-secondary">View</button>
-                  <button type="button" class="btn btn-sm btn-outline-secondary">Edit</button>
-                </div>
-                <small class="text-body-secondary">9 mins</small>
-              </div>
-            </div>
-          </div>
-        </div>
-        <div class="col">
-          <div class="card shadow-sm">
-            <svg class="bd-placeholder-img card-img-top" width="100%" height="225" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: Thumbnail" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#55595c"></rect><text x="50%" y="50%" fill="#eceeef" dy=".3em">Thumbnail</text></svg>
-            <div class="card-body">
-              <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-              <div class="d-flex justify-content-between align-items-center">
-                <div class="btn-group">
-                  <button type="button" class="btn btn-sm btn-outline-secondary">View</button>
-                  <button type="button" class="btn btn-sm btn-outline-secondary">Edit</button>
-                </div>
-                <small class="text-body-secondary">9 mins</small>
-              </div>
-            </div>
-          </div>
-        </div>
-        <div class="col">
-          <div class="card shadow-sm">
-            <svg class="bd-placeholder-img card-img-top" width="100%" height="225" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: Thumbnail" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#55595c"></rect><text x="50%" y="50%" fill="#eceeef" dy=".3em">Thumbnail</text></svg>
-            <div class="card-body">
-              <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-              <div class="d-flex justify-content-between align-items-center">
-                <div class="btn-group">
-                  <button type="button" class="btn btn-sm btn-outline-secondary">View</button>
-                  <button type="button" class="btn btn-sm btn-outline-secondary">Edit</button>
-                </div>
-                <small class="text-body-secondary">9 mins</small>
-              </div>
-            </div>
-          </div>
-        </div>
-</body>    
