11f7d6b3671e5a1f4823ffac8b9d3c254301aed2
flouksac <flouksac1@gmail.com>
2023-10-16 19:26:25 +0200

    ajout htaccess

--- views/menu/homeFeed.php
+++ views/menu/homeFeed.php
@@ -70,7 +70,7 @@
         <div class="row row-cols-1 row-cols-md-3 mb-3 text-center">
             <div class="col">
                 <div class="card mb-4 rounded-4 shadow-gn background">
-                    <a href="https://cyphub.tech/ActualityFeedController/" type="button" class="w-20 rounded-4 btn btn-lg text-center colorButton shadow-gn grow-button">
+                    <a href="./../ActualityFeed/" type="button" class="w-20 rounded-4 btn btn-lg text-center colorButton shadow-gn grow-button">
                         <img class="sizeRs" src="../../media/public_assets/iconeTech.png"alt="Icone Veille Tehcnologique " height="150" width="150">
                         <p class="colorText">Veille Technologique</p></a>
                 </div>
