e4061b9ce3d105229dc985a16a6fdd97ece2a7cc
Pierrick Caron <pierrick.caron@etu.univ-amu.fr>
2023-10-16 14:43:12 +0200

    Modification du homeFeed + Début de la page de veille technologique

--- views/menu/technologyWatchFeed.php
+++ views/menu/technologyWatchFeed.php
@@ -1,3 +1,21 @@
-<?php require "views/header.php";
-require "common_scripts/general_scripts.php";
-?>
+<head>
+    <link rel="stylesheet" href="../../common_styles/technologyWatchFeedStyle.css">
+</head>
+<body>
+<main class="container padding">
+    <a class="btn bg-body-tertiary p-5 round background shadow d-flex align-items-center " role="button">
+            <img src="../../media/public_assets/terminalLogo.png" height="250" width="250">
+            <div class="bg-body-tertiary p-5 round">
+                <h1>Le logiciel espion Pegasus cible de nouveau les iPhones  </h1>
+                <p class="autor"> Iphone / IOS - 08 september 2023 - auteur </p>
+                <p class="lead">Apple a colmaté une brèche exploitée par le spyware Pegasus. Devenu célèbre dans le cadre d'une
+                    affaire d'espionnage mondial de personnalités publiques, il permet d'épier les utilisateurs d'iPhones à leur insu.
+                    Le laboratoire canadien Citizen Lab, à l'origine de cette découverte, exhorte les détenteurs les plus sensibles à
+                    activer le mode verrouillage proposé par la société américaine visant à réduire la surface d'attaque de ses appareils.</p>
+            </div>
+    </a>
+</main>
+<script src="/docs/5.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+
+
+</body>
