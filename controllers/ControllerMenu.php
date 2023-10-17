56f78569152d0a8f8bced2a257f5ed16c69f5808
Tom Carvajal <tom.carvajal@etu.univ-amu.fr>
2023-10-17 15:11:35 +0200

    test param pour routocontroller

--- controllers/ControllerMenu.php
+++ controllers/ControllerMenu.php
@@ -6,14 +6,16 @@ class ControllerMenu
     {
         MotorView::show('menu/homeFeed');
     }
-    public function ActualityFeedAction(): void
+    public function ActualityFeedAction(Array $A_parametres = null, Array $A_postParams = null): void
     {
+        print_r($A_parametres);
+        print_r($A_postParams);
         //TO-DO : récupérer les paramètres (afin de charger d'autres Articles)
         //la requete ressemble à : GET /Menu/ActualityFeed/More
 
         MotorView::show('menu/actualityFeed');
     }
-    public function BlogFeedAction(): void
+    public function BlogFeedAction(Array $A_parametres = null, Array $A_postParams = null): void
     {
         //TO-DO : récupérer les paramètres (afin de charger d'autres Blogs)
         //la requete ressemble à : GET /Menu/BlogFeed/More
@@ -28,7 +30,7 @@ class ControllerMenu
         //la requete ressemble à :  POST /Menu/BlogFeed/Filter
         MotorView::show('menu/blogFeed');
     }
-    public function ForumFeedAction(): void
+    public function ForumFeedAction(Array $A_parametres = null, Array $A_postParams = null): void
     {
         //TO-DO : récupérer les paramètres (afin de charger d'autres Forums)
         //la requete ressemble à : GET /Menu/ForumFeed/More
