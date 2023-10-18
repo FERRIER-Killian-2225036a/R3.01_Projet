e97a93cc59e36bd8a54f4e98a8b9489c34886003
Tom Carvajal <tom.carvajal@etu.univ-amu.fr>
2023-10-18 13:07:52 +0200

    annulation des tests d'authentification (on va plutot essayer de faire différement

--- index.php
+++ index.php
@@ -26,25 +26,6 @@
     $mapOfPostParameters = isset($_POST) ? $_POST : null;
     print_r($S_urlADecortiquer);
 
-
-    session_start();
-
-    if ($S_urlADecortiquer != '' && $S_urlADecortiquer != '/' && $S_urlADecortiquer != 'Menu' &&
-        $S_urlADecortiquer != 'Menu/HomeFeed' && $S_urlADecortiquer != 'Menu/ActualityFeed' &&
-        $S_urlADecortiquer != 'Menu/BlogFeed' && $S_urlADecortiquer != 'Menu/ForumFeed' &&
-        $S_urlADecortiquer != 'Auth' && $S_urlADecortiquer != 'Auth/Login' &&
-        $S_urlADecortiquer != 'Auth/SignUp' )
-    {
-        if (!isset($_SESSION['user'])) {
-            // L'utilisateur n'est pas authentifié, redirige vers la page de connexion
-
-            header('Location: /Auth/Login');
-            echo "on doit te rediriger";
-
-            exit;
-        }
-    }
-
     try {
         $conn = DatabaseManager::getInstance();
     }
@@ -52,22 +33,29 @@
         echo ('Une erreur s\'est produite : ' . $O_exception->getMessage());
     }
 
+    //session_start();
 
 
-    MotorView::openBuffer(); // on ouvre le tampon d'affichage, les contrôleurs qui appellent des vues les mettront dedans
-    try
-    {
-        $O_controleur = new MotorController($S_urlADecortiquer, $mapOfPostParameters);
-        $O_controleur->run();
-    }
-    catch (ExceptionsController $O_exception)
-    {
-        echo ('Une erreur s\'est produite : ' . $O_exception->getMessage());
-    }
+
+        MotorView::openBuffer(); // on ouvre le tampon d'affichage, les contrôleurs qui appellent des vues les mettront dedans
+        try
+        {
+            $O_controleur = new MotorController($S_urlADecortiquer, $mapOfPostParameters);
+            $O_controleur->run();
+        }
+        catch (ExceptionsController $O_exception)
+        {
+            echo ('Une erreur s\'est produite : ' . $O_exception->getMessage());
+        }
 
 
-    // Les différentes sous-vues ont été "crachées" dans le tampon d'affichage, on les récupère
-    $contentForShow = MotorView::getBufferContent();
+        // Les différentes sous-vues ont été "crachées" dans le tampon d'affichage, on les récupère
+        $contentForShow = MotorView::getBufferContent();
 
-    // On affiche le contenu dans la partie body du gabarit général
-    MotorView::show('main', array('body' => $contentForShow));
+        // On affiche le contenu dans la partie body du gabarit général
+        MotorView::show('main', array('body' => $contentForShow));
+
+
+
+
+
