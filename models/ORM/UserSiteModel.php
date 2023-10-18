98155e1f08818f8b4473a5421d61bb3dd745f2ba
Tom Carvajal <tom.carvajal@etu.univ-amu.fr>
2023-10-18 15:09:01 +0200

    debug message erreur signup

--- models/ORM/UserSiteModel.php
+++ models/ORM/UserSiteModel.php
@@ -61,7 +61,7 @@ class UserSiteModel
         $checkUserSQL = "SELECT COUNT(*) FROM USERSite WHERE Mail = ? OR Pseudo = ?";
         $stmt = $this->conn->prepare($checkUserSQL);
         $stmt->bindParam(1, $mail_a, PDO::PARAM_STR);
-        $stmt->bindParam(2, $$pseudo_a, PDO::PARAM_STR);
+        $stmt->bindParam(2, $pseudo_a, PDO::PARAM_STR);
         $stmt->execute();
         $count = $stmt->fetchColumn();
         echo $count > 0;
