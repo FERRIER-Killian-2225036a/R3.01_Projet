f7998f36eeb0c26b0bdcff98f2b9775104c8de97
Tom Carvajal <tom.carvajal@etu.univ-amu.fr>
2023-10-18 14:47:47 +0200

    correctif sécurisation input niveau model (verif si user existe deja) verif force mdp si leak

--- models/ORM/UserSiteModel.php
+++ models/ORM/UserSiteModel.php
@@ -14,9 +14,19 @@ class UserSiteModel
     public function createUser($pseudo_a,$mail_a,  $password_a) {
         try {
             // Test le format d'un email
+
+
             if (!$this->DBBrain->isValidEmail($mail_a)) {
-                throw new Exception("Invalid email format");
+                throw new ExceptionsDatabase("Invalid email format");
             }
+            if ($this->isUserExists($mail_a, $pseudo_a)) {
+                throw new ExceptionsDatabase("User with this email or pseudo already exists");
+            }
+            if ($this->isPasswordSafe($password_a)) {
+                throw new ExceptionsDatabase("Ce mot de passe n'est pas assez sécurisé");
+            }
+            
+
             $this->conn->beginTransaction();
             // Insert user into USERSite
             $insertUserSQL = "INSERT INTO USERSite (Mail, Pseudo, DateFirstLogin, DateLastLogin, Role, AlertLevelUser, NumberOfAction, Status, lastIpAdress, NumberOfConnection) VALUES (?, ?, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP, 'registered', 0, 0, 'connected', ?, 1)";
@@ -25,7 +35,7 @@ class UserSiteModel
             $stmt1->bindParam(2, $pseudo_a, PDO::PARAM_STR);
             $stmt1->bindParam(3, $_SERVER['REMOTE_ADDR'], PDO::PARAM_STR);
             $stmt1->execute();
-            // Get UserId
+            // recupération de UserId
             $userId = $this->conn->lastInsertId();
             // Insert password into PASSWORD
             $insertPasswordSQL = "INSERT INTO PASSWORD (Password, UserId) VALUES (?, ?)";
@@ -33,17 +43,52 @@ class UserSiteModel
             $stmt2->bindParam(1, $password_a, PDO::PARAM_STR);
             $stmt2->bindParam(2, $userId, PDO::PARAM_INT);
             $stmt2->execute();
-            // Commit the transaction
+            // Commit de la transaction
             $this->conn->commit();
-            // Return the UserId
+            // renvoi l'identifiant du nouvel utilisateur
             return $userId;
-        } catch (Exception $e) {
-            // In case of an error, rollback the transaction
-            //$this->conn->rollback();
-            echo "Error creating user: " . $e->getMessage();
-            echo "$mail_a, $pseudo_a, $password_a" ;
-            return null;
+        } catch (ExceptionsDatabase $e) {
+            //annulation de la transaction si erreur
+            $this->conn->rollback();
+            //echo "Error creating user: " . $e->getMessage();
+            //echo "$mail_a, $pseudo_a, $password_a" ;
+            return false;
         }
     }
 
+    private function isUserExists($mail_a, $pseudo_a): bool
+    {
+        $checkUserSQL = "SELECT COUNT(*) FROM USERSite WHERE Mail = ? OR Pseudo = ?";
+        $stmt = $this->conn->prepare($checkUserSQL);
+        $stmt->bindParam(1, $email, PDO::PARAM_STR);
+        $stmt->bindParam(2, $pseudo, PDO::PARAM_STR);
+        $stmt->execute();
+        $count = $stmt->fetchColumn();
+        return $count > 0;
+
+    }
+
+    private function isPasswordSafe($password_a): bool
+    {    $hashedPassword = strtoupper(sha1($password_a));
+
+        // Prenez les 5 premiers caractères du hachage (préfixe)
+        $prefix = substr($hashedPassword, 0, 5);
+
+        // Faites une requête à l'API Have I Been Pwned
+        $apiUrl = "https://api.pwnedpasswords.com/range/" . $prefix;
+        $response = file_get_contents($apiUrl);
+
+        // Recherchez le reste du hachage dans la réponse
+        $searchTerm = substr($hashedPassword, 5);
+        $searchResult = preg_match('/' . $searchTerm . ':(\d+)/', $response, $matches);
+
+        // Si le mot de passe est apparu dans une fuite, retournez false
+        if ($searchResult) {
+            return false;
+        }
+
+        // Si le mot de passe est suffisamment fort, retournez true
+        // Vous pouvez ajouter d'autres critères de force ici
+        return true;
+    }
 }
