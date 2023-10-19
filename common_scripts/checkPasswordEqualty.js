730cbcaec266f542c796108f01ca7440f44e0231
Pierrick Caron <pierrick.caron@etu.univ-amu.fr>
2023-10-19 10:26:43 +0200

    Modification de checkPasswordEqualty.js pour qui puisse faire la vérification en temps réel

--- common_scripts/checkPasswordEqualty.js
+++ common_scripts/checkPasswordEqualty.js
@@ -2,11 +2,15 @@ function checkPasswordsEquality() {
     const password = document.getElementById('password').value;
     const confirmPassword = document.getElementById('confirmPassword').value;
 
-    if (password === confirmPassword) {
-        document.getElementById('message').textContent = "Les mots de passe correspondent !";
-        document.getElementById('message').style.color = "green";
+    if (password && confirmPassword) { // On vérifie si les deux champs ont des valeurs pour éviter un feedback inutile lorsque les champs sont vides
+        if (password === confirmPassword) {
+            document.getElementById('message').textContent = "Les mots de passe correspondent !";
+            document.getElementById('message').style.color = "green";
+        } else {
+            document.getElementById('message').textContent = "Les mots de passe ne correspondent pas !";
+            document.getElementById('message').style.color = "red";
+        }
     } else {
-        document.getElementById('message').textContent = "Les mots de passe ne correspondent pas !";
-        document.getElementById('message').style.color = "red";
+        document.getElementById('message').textContent = ""; // Efface le message s'il n'y a pas de valeur dans l'un des champs
     }
 }
