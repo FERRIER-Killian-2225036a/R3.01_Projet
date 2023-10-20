6a7546a6d3de9cddeccdbe59c309c47aa59c8d18
Killian Ferrier <killian.ferrier@etu.univ-amu.fr>
2023-10-20 19:19:19 +0200

    Ajout de l'id de l'utilisateur responsable des news de la page veille tech

--- config/Constants.php
+++ config/Constants.php
@@ -7,6 +7,8 @@ final class Constants
     const EXCEPTION_DIRECTORY  = '/monitoring/exceptions/';
     const CONTROLLER_DIRECTORY = '/controllers/';
     const ORM_DIRECTORY = '/models/ORM/';
+    const UserIdForItArticle = 238;
+
 
     const DB = array(
         "dbname"=>"cyphubte_db",
