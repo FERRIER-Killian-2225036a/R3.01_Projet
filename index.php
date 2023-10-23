d62c1b3b5ac10b0f53363a4aaf770ad74d84e72a
Tom Carvajal <tom.carvajal@etu.univ-amu.fr>
2023-10-23 10:36:15 +0200

    debug mdp bdd

--- index.php
+++ index.php
@@ -3,6 +3,7 @@
 
     require 'core/AutoLoad.php';
     Constants::defineFakeConst();
+    echo Constants::$DB['host'];
     error_log(print_r(Constants::$DB, true));
     print_r(Constants::$DB);
 
