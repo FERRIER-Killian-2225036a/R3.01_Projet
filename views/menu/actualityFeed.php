ae4042c252ec2045a7ba140adbfa83ec4c555486
Killian Ferrier <killian.ferrier@etu.univ-amu.fr>
2023-10-23 11:49:16 +0200

    test actuality post

--- views/menu/actualityFeed.php
+++ views/menu/actualityFeed.php
@@ -17,19 +17,6 @@
                     Le laboratoire canadien Citizen Lab, à l'origine de cette découverte, exhorte les détenteurs les plus sensibles à
                     activer le mode verrouillage proposé par la société américaine visant à réduire la surface d'attaque de ses appareils.</p>
             </div>
-        </a>-->
-    </div>
-    <div class="p-3">
-        <a class="btn bg-body-tertiary round background grow-button" role="button">
-            <img src="https://www.numerama.com/wp-content/uploads/2023/09/iphone15-3131-1.jpg" alt="Logo" class="responsive-image round p-1">
-            <div class="text-content">
-                <h1 class="responsiveTitle" >Le logiciel espion Pegasus cible de nouveau les iPhones</h1>
-                <p class="lead responsiveText"> Iphone / IOS - 08 september 2023 - auteur </p>
-                <p class="responsiveText">Apple a colmaté une brèche exploitée par le spyware Pegasus. Devenu célèbre dans le cadre d'une
-                    affaire d'espionnage mondial de personnalités publiques, il permet d'épier les utilisateurs d'iPhones à leur insu.
-                    Le laboratoire canadien Citizen Lab, à l'origine de cette découverte, exhorte les détenteurs les plus sensibles à
-                    activer le mode verrouillage proposé par la société américaine visant à réduire la surface d'attaque de ses appareils.</p>
-            </div>
         </a>
     </div>
     <div class="p-3">
@@ -45,6 +32,20 @@
             </div>
         </a>
     </div>
+    <div class="p-3">
+        <a class="btn bg-body-tertiary round background grow-button" role="button">
+            <img src="https://www.numerama.com/wp-content/uploads/2023/09/iphone15-3131-1.jpg" alt="Logo" class="responsive-image round p-1">
+            <div class="text-content">
+                <h1 class="responsiveTitle" >Le logiciel espion Pegasus cible de nouveau les iPhones</h1>
+                <p class="lead responsiveText"> Iphone / IOS - 08 september 2023 - auteur </p>
+                <p class="responsiveText">Apple a colmaté une brèche exploitée par le spyware Pegasus. Devenu célèbre dans le cadre d'une
+                    affaire d'espionnage mondial de personnalités publiques, il permet d'épier les utilisateurs d'iPhones à leur insu.
+                    Le laboratoire canadien Citizen Lab, à l'origine de cette découverte, exhorte les détenteurs les plus sensibles à
+                    activer le mode verrouillage proposé par la société américaine visant à réduire la surface d'attaque de ses appareils.</p>
+            </div>
+        </a>
+    </div>
+    -->
 </main>
 
 <script src="/docs/5.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
