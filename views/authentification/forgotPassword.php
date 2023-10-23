a2f3903d445725388b82441e1b6a1fada878aef0
Pierrick Caron <pierrick.caron@etu.univ-amu.fr>
2023-10-23 17:13:55 +0200

    Page forgotPassword.php quasiment fini, manque plus que la gestion de  la taille des textes

--- views/authentification/forgotPassword.php
+++ views/authentification/forgotPassword.php
@@ -1,36 +1,26 @@
+<head>
+    <link rel="stylesheet" href="../../common_styles/authentification.css">
+    <link rel="stylesheet" href="../../common_styles/general.css">
+</head>
 <body>
 
-<div class="modal fade" id="passwordModal">
-    <div class="modal-dialog">
-        <div class="modal-content">
-
-            <!-- Header de la modale -->
-            <div class="modal-header">
-                <h4 class="modal-title">Mot de passe oublié</h4>
-                <button type="button" class="close" data-dismiss="modal">&times;</button>
-            </div>
-
-            <!-- Corps de la modale -->
-            <div class="modal-body">
-                <p>Entrez l'adresse e-mail associée à votre compte, et nous vous enverrons un lien pour réinitialiser votre mot de passe.</p>
-                <form action="/path-to-your-handler" method="POST">
-                    <!-- Adresse e-mail -->
-                    <div class="form-group">
-                        <label for="email">Adresse e-mail</label>
-                        <input type="email" class="form-control" id="email" name="email" required>
-                    </div>
-                    <!-- Bouton pour valider -->
-                    <div class="form-group">
-                        <button type="submit" class="btn btn-primary">Envoyer le lien de réinitialisation</button>
-                    </div>
-                </form>
-            </div>
-
-            <!-- Footer de la modale -->
-            <div class="modal-footer">
-                <button type="button" class="btn btn-danger" data-dismiss="modal">Fermer</button>
-            </div>
-
+<div class="container mt-5">
+    <div class="row">
+        <div class="col-md-6 offset-md-3 p-5 shadow-lg round">
+            <h2 class="text-center responsive-title">Mot de passe oublié</h2>
+            <br>
+            <p class="responsive-text">Entrez l'adresse e-mail associée à votre compte, et nous vous enverrons un lien pour réinitialiser votre mot de passe.</p>
+            <form action="/path-to-your-handler" method="POST">
+                <!-- Adresse e-mail -->
+                <div class="form-group responsive-text">
+                    <label for="email">Adresse e-mail</label>
+                    <input type="email" class="form-control" id="email" name="email" placeholder="Entrez l'adresse mail associée au compte" required>
+                </div>
+                <!-- Bouton pour valider -->
+                <div class="form-group pt-2">
+                    <button type="submit" class="btn btn-outline-danger btn-custom-border ">Envoyer le lien de réinitialisation</button>
+                </div>
+            </form>
         </div>
     </div>
 </div>
--- views/authentification/login.php
+++ views/authentification/login.php
@@ -35,9 +35,7 @@
 
 
                                     <div class="text-center pt-1 mb-5 pb-1">
-                                        <button type="button" class="btn" data-toggle="modal" data-target="#passwordModal">
-                                            Mot de passe oublié?
-                                        </button>
+                                        <a id="forgotPasswordSize" href="#!">Mot de passe oublié ?</a>
                                         <br>
                                         <button class="btn btn-primary btn-block fa-lg gradient-custom-2 mb-3" id="signUpButton" type="submit" >Se connecter</button>
                                     </div>
