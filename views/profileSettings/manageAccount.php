a76a0ebc7a6bc77fcab875fd30c5add0876596c3
Pierrick Caron <pierrick.caron@etu.univ-amu.fr>
2023-10-26 13:50:41 +0200

    Ajout des fonctionnalités qui test la difficulté du mot de passe et test de l'égalité des mots de passe

--- views/profileSettings/manageAccount.php
+++ views/profileSettings/manageAccount.php
@@ -1,4 +1,7 @@
 <?php require 'sideBar.php'; ?>
+<link rel="stylesheet" href="../../common_styles/authentification.css"
+<script src="/common_scripts/showErrorMessage.js"></script>
+<script src="/common_scripts/checkPassword.js"></script>
         <div class="col container">
 
             <!-- Section pour la photo de profil -->
@@ -44,14 +47,14 @@
 
             <div class="row mb-2">
                 <div class="col-md-6">
-                    <h2>Pseudo</h2>
+                    <h2>Changer votre pseudo</h2>
                 </div>
             </div>
             <div class="row mb-4">
-                <form action="/Settings/ManageAccount" method="POST">
+               <form action="/Settings/ManageAccount" method="POST">
+                   <label for="username-input"></label>
                 <div class="col-md-6">
-                    <label for="username-input">Entrez votre nouveau pseudo</label>
-                    <input type="text" class="form-control" name="username" id="username-input" value="VotrePseudo">
+                    <input type="text" class="form-control" name="username" id="username-input" placeholder="NouveauPseudo">
                 </div>
                 <div class="col-md-6 d-flex align-items-center"> <!-- Notez ici que nous utilisons align-items-center pour centrer les boutons verticalement -->
                     <div class="btn-group">
@@ -66,23 +69,25 @@
             <div class="row mb-2">
                 <div class="col-md-6">
                     <h2>Modifier l'adresse mail</h2>
+                    <label for="email-input"></label>
                 </div>
             </div>
             <div class="row mb-4">
                 <div class="col-md-6">
-                    <input type="email" class="form-control" id="email-input" value="votre@email.com">
+                    <input type="email" class="form-control" id="email-input" placeholder="votre@email.com">
                 </div>
                 <div class="col-md-6 d-flex align-items-center"> <!-- Notez ici que nous utilisons align-items-center pour centrer les boutons verticalement -->
                     <div class="btn-group">
-                        <button class="btn btn-outline-dark" onclick="updateEmail()">Modifier</button>
+                        <button class="btn btn-outline-dark">Modifier</button>
                     </div>
                 </div>
             </div>
-            <!-- Section pour le mot de passe -->
 
+            <!-- Section pour le mot de passe -->
             <div class="row mb-2">
-                <div class="col-md-12">
+                <div class="col-md-6">
                     <h2>Modifier le mot de passe</h2>
+                    <label for="password-input"></label>
                 </div>
             </div>
             <div class="row mb-2">
@@ -90,12 +95,15 @@
                     <div class="input-group mb-3">
                         <input type="password" class="form-control" id="password-input" placeholder="Ancien mot de passe">
                     </div>
+                    <label for="passwordStrength"></label>
                     <div class="input-group mb-3">
-                        <input type="password" class="form-control" id="new-password-input" placeholder="Nouveau mot de passe">
+                        <input type="password" class="form-control" id="passwordStrength" placeholder="********" name="password" onkeyup="checkPasswordStrength(this.value)" />
                     </div>
+                    <label for="confirmPassword"></label>
                     <div class="input-group">
-                        <input type="password" class="form-control" id="confirm-new-password-input" placeholder="Confirmer le nouveau mot de passe">
+                        <input type="password" id="confirmPassword" class="form-control" oninput="checkPasswordsEquality()" placeholder="********"/>
                     </div>
+                    <span id="passwordFeedback"></span>
                 </div>
                 <div class="col-md-6 d-flex align-items-end justify-content-start">
                     <button class="btn btn-custom" onclick="updatePassword()">Modifier</button>
