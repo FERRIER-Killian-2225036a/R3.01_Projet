02199697b857efc8898b611b728476f3010c7f6a
Tom Carvajal <tom.carvajal@etu.univ-amu.fr>
2023-10-26 17:20:00 +0200

    lien bouton/controller

--- views/profileSettings/sideBar.php
+++ views/profileSettings/sideBar.php
@@ -8,7 +8,7 @@
         <div class="d-flex flex-column flex-shrink-0 shadow round mt-3 ml-custom p-2" style="width: 280px;"> <!-- Ajout de la classe 'shadow' ici -->
 
             <!-- Mon compte -->
-            <a href="/" class="d-flex align-items-center mb-3 mb-md-0 me-md-auto link-body-emphasis text-decoration-none text-dark"> <!-- Ajout de 'text-dark' ici -->
+            <a href="/Settings/" class="d-flex align-items-center mb-3 mb-md-0 me-md-auto link-body-emphasis text-decoration-none text-dark"> <!-- Ajout de 'text-dark' ici -->
                 <img src="<?= (SessionManager::isUserConnected() && $_SESSION['UrlPicture']!==null ) ? $_SESSION['UrlPicture'] : Constants::PDP_URL_DEFAULT; ?>" alt="Profil Image" width="40" height="40" class="rounded-circle me-2">
                 <span class="fs-4">Mon compte</span>
             </a>
@@ -16,57 +16,57 @@
             <ul class="nav nav-pills flex-column mb-auto align-items-center">
                 <!-- Information personnel-->
                 <li class="nav-item">
-                    <a href="manageAccount.php" class="nav-link custom-nav-link text-dark"> <!-- Ajout de 'text-dark' ici -->
+                    <a href="/Settings/ManageAccount" class="nav-link custom-nav-link text-dark"> <!-- Ajout de 'text-dark' ici -->
                         Informations personnelles
                     </a>
                 </li>
                 <!-- Support -->
                 <li>
-                    <a href="support.php" class="nav-link custom-nav-link text-dark"> <!-- Ajout de 'text-dark' ici -->
+                    <a href="/Settings/Support" class="nav-link custom-nav-link text-dark"> <!-- Ajout de 'text-dark' ici -->
                         Support
                     </a>
                 </li>
                 <!-- Langue -->
                 <li>
-                    <a href="language.php" class="nav-link custom-nav-link text-dark"> <!-- Ajout de 'text-dark' ici -->
+                    <a href="/Settings/Language" class="nav-link custom-nav-link text-dark"> <!-- Ajout de 'text-dark' ici -->
                         Langue
                     </a>
                 </li>
                 <!-- Thème -->
                 <li>
-                    <a href="theme.php" class="nav-link custom-nav-link text-dark"> <!-- Ajout de 'text-dark' ici -->
+                    <a href="/Settings/Theme" class="nav-link custom-nav-link text-dark"> <!-- Ajout de 'text-dark' ici -->
                         Thème
                     </a>
                 </li>
                 <hr class="my-3">
                 <!--Mes enregistrements -->
                 <li class="nav-item">
-                    <a href="bookmark.php" class="nav-link custom-nav-link text-dark"> <!-- Ajout de 'text-dark' ici -->
+                    <a href="/Settings/Bookmark" class="nav-link custom-nav-link text-dark"> <!-- Ajout de 'text-dark' ici -->
                         Mes enregistrements
                     </a>
                 </li>
                 <!-- Personnes suivies -->
                 <li>
-                    <a href="follow.php" class="nav-link custom-nav-link text-dark"> <!-- Ajout de 'text-dark' ici -->
+                    <a href="/Settings/Follow" class="nav-link custom-nav-link text-dark"> <!-- Ajout de 'text-dark' ici -->
                         Personnes suivies
                     </a>
                 </li>
                 <!-- Mes commentaires -->
                 <li>
-                    <a href="myComment.php" class="nav-link custom-nav-link text-dark"> <!-- Ajout de 'text-dark' ici -->
+                    <a href="/Settings/MyComment" class="nav-link custom-nav-link text-dark"> <!-- Ajout de 'text-dark' ici -->
                         Mes commentaires
                     </a>
                 </li>
                 <!-- Mes posts -->
                 <li>
-                    <a href="myPost.php" class="nav-link custom-nav-link text-dark"> <!-- Ajout de 'text-dark' ici -->
+                    <a href="/Settings/MyPost" class="nav-link custom-nav-link text-dark"> <!-- Ajout de 'text-dark' ici -->
                         Mes posts
                     </a>
                 </li>
             </ul>
             <hr class="hr-color">
-            <a href="#" class="d-flex align-items-center justify-content-center mb-3 link-body-emphasis text-danger text-decoration-none">
-                <img src="../../media/public_assets/icone/iconeSeDeconnecter.png" alt="Logo de déconnexion" class="me-2">
+            <a href="/Auth/LogOut" class="d-flex align-items-center justify-content-center mb-3 link-body-emphasis text-danger text-decoration-none">
+                <img src="/media/public_assets/icone/iconeSeDeconnecter.png" alt="Logo de déconnexion" class="me-2">
                 Se déconnecter
             </a>
 
