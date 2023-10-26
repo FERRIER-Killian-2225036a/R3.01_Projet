91e26d4a9ae9d71e11e9c5a0127273950fe190f3
Pierrick Caron <pierrick.caron@etu.univ-amu.fr>
2023-10-26 14:17:35 +0200

    Début de la page de support.php

--- views/profileSettings/support.php
+++ views/profileSettings/support.php
@@ -1,1 +1,15 @@
-<?php
+<?php require 'sideBar.php' ?>
+<div class="col container">
+
+    <!-- Section pour la photo de profil -->
+    <div class="row mb-2 mt-2">
+        <div class="col-md-6">
+            <h1>Demande de support</h1>
+        </div>
+        <div class="col-md-6 d-flex align-items-center justify-content-center"> <!-- Notez ici que nous utilisons align-items-center pour centrer les boutons verticalement -->
+            <div class="btn-group">
+                <button class="btn btn-outline-dark" name="ChangeUsername" value="1" type="submit">Créer un ticket</button>
+            </div>
+        </div>
+        <hr class="my-3">
+
