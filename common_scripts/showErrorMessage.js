926f0edd1864130cae3f64b437452665328cded4
Killian Ferrier <killian.ferrier@etu.univ-amu.fr>
2023-10-26 11:52:55 +0200

    Réglage de bugs

--- common_scripts/showErrorMessage.js
+++ common_scripts/showErrorMessage.js
@@ -1,9 +1,11 @@
 function ShowLoginErrorMessage(status) {
     const errorMessage = document.getElementById('wrongInfo');
+    let errorMessageText = errorMessage.textContent;
+    console.log(errorMessageText);
     if (status!=="success") {
         errorMessage.style.display = 'inherit';
     } else {
-        errorMessage.innerHTML = status;
+        errorMessageText.innerHTML = status;
         errorMessage.style.display = 'none';
     }
 }
