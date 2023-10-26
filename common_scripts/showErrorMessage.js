e2361c24974aaf299f48a57620cbe44ccba65fc1
Killian Ferrier <killian.ferrier@etu.univ-amu.fr>
2023-10-26 15:20:32 +0200

    Bug résolu

--- common_scripts/showErrorMessage.js
+++ common_scripts/showErrorMessage.js
@@ -1,11 +1,8 @@
 function ShowLoginErrorMessage(status) {
     const errorMessage = document.getElementById('wrongInfo');
-    errorMessage.style.display = 'inherit';
-    console.log(status);
+    errorMessage.style.display = 'none';
     if (status!=="success") {
-        console.log("Avant" + errorMessage.textContent);
         errorMessage.textContent = status;
-        console.log("Après" + errorMessage.textContent);
         errorMessage.style.display = 'inherit';
     } else {
         errorMessage.style.display = 'none';
