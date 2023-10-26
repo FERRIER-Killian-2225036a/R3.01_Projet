e453ea2feca171a3cd0ccf07afb4128b30e48ed6
Killian Ferrier <killian.ferrier@etu.univ-amu.fr>
2023-10-26 11:43:47 +0200

    Affichage des erreurs en fonction de l'erreur

--- common_scripts/showErrorMessage.js
+++ common_scripts/showErrorMessage.js
@@ -4,6 +4,7 @@ function ShowLoginErrorMessage(status) {
     if (status!=="success") {
         errorMessage.style.display = 'inherit';
     } else {
+        errorMessage.innerHTML = status;
         errorMessage.style.display = 'none';
     }
 }
