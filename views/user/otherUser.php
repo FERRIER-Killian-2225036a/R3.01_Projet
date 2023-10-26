04c7bce678c6bd1e368ea45753629762b204927d
Pierrick Caron <pierrick.caron@etu.univ-amu.fr>
2023-10-26 17:02:06 +0200

    Début de la page otherUser.php

--- views/user/otherUser.php
+++ views/user/otherUser.php
@@ -1,1 +1,73 @@
-<?php
+<link rel="stylesheet" href="../../common_styles/user.css">
+<link rel="stylesheet" href="../../common_styles/general.css">
+<main class="container">
+    <div class="row mb-4">
+        <div class="col-md-6 d-flex align-items-center">
+            <img src="../../media/public_assets/favicon.png" alt="image" class="rounded-circle mr-3">
+            <div>
+                <p class="mb-0">KrokmouLeFurynocturne</p>
+                <small>99 Abonnés     99 Abonnements     1K Posts</small>
+                <button class="btn btn-outline-dark">Se désabonnez</button>
+            </div>
+        </div>
+        <div class="col-md-6 d-flex align-items-center justify-content-end">
+            <div class="dropdown">
+                <button type="button" class="btn btn-primary">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-filter" viewBox="0 0 16 16">
+                        <path d="M6 10.5a.5.5 0 0 1 .5-.5h3a.5.5 0 0 1 0 1h-3a.5.5 0 0 1-.5-.5zm-2-3a.5.5 0 0 1 .5-.5h7a.5.5 0 0 1 0 1h-7a.5.5 0 0 1-.5-.5zm-2-3a.5.5 0 0 1 .5-.5h11a.5.5 0 0 1 0 1h-11a.5.5 0 0 1-.5-.5z"></path>
+                    </svg>
+                    Filtrer
+                </button>
+                <ul class="dropdown-menu">
+                    <input class="form-control" id="myInput" type="text" placeholder="Search..">
+                    <li><a href="#">Data</a></li>
+                    <li><a href="#">Cybersec</a></li>
+                    <li><a href="#">IDK</a></li>
+                </ul>
+            </div>
+        </div>
+    </div>
+
+    <!-- Articles -->
+    <div class="row">
+        <!-- Premier élément -->
+        <div class="col-md-4 p-3">
+            <a class="btn bg-body-tertiary round background grow-button d-block" role="button" href="#" target="_blank">
+                <div class="d-flex justify-content-center mb-2">
+                    <img src="../../media/public_assets/imageTest.jpeg" alt="Logo" class="responsive-image-user round p-1">
+                </div>
+                <div class="text-content">
+                    <h1 class="responsiveTitle">Informatique nous rend dépendant</h1>
+                    <p class="lead responsiveText">Catégorie - 00-00-00 - Par l'auteur</p>
+                    <p class="responsiveText">ça va parler chinois : Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit neskwik... VOIR PLUS</p>
+                </div>
+            </a>
+        </div>
+        <!-- Deuxième élément -->
+        <div class="col-md-4 p-3">
+            <a class="btn bg-body-tertiary round background grow-button d-block" role="button" href="#" target="_blank">
+                <div class="d-flex justify-content-center mb-2">
+                    <img src="../../media/public_assets/imageTest.jpeg" alt="Logo" class="responsive-image-user round p-1">
+                </div>
+                <div class="text-content">
+                    <h1 class="responsiveTitle">Informatique nous rend dépendant</h1>
+                    <p class="lead responsiveText">Catégorie - 00-00-00 - Par l'auteur</p>
+                    <p class="responsiveText">ça va parler chinois : Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit neskwik... VOIR PLUS</p>
+                </div>
+            </a>
+        </div>
+        <!-- Troisième élément -->
+        <div class="col-md-4 p-3">
+            <a class="btn bg-body-tertiary round background grow-button d-block" role="button" href="#" target="_blank">
+                <div class="d-flex justify-content-center mb-2">
+                    <img src="../../media/public_assets/imageTest.jpeg" alt="Logo" class="responsive-image-user round p-1">
+                </div>
+                <div class="text-content">
+                    <h1 class="responsiveTitle">Informatique nous rend dépendant</h1>
+                    <p class="lead responsiveText">Catégorie - 00-00-00 - Par l'auteur</p>
+                    <p class="responsiveText">ça va parler chinois : Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit neskwik... VOIR PLUS</p>
+                </div>
+            </a>
+        </div>
+    </div>
+</main>
