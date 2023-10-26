281c456c654e610f7a562a48225b3fe8985c0c16
Pierrick Caron <pierrick.caron@etu.univ-amu.fr>
2023-10-26 14:07:09 +0200

    theme.php fini plus qu'à faire des test pour le dropdown pareil que pour language.php

--- views/profileSettings/theme.php
+++ views/profileSettings/theme.php
@@ -1,1 +1,20 @@
-<?php
+<?php require "sideBar.php"; ?>
+    <body>
+    <div class="col-md-6">
+        <h1 class="responsive-title">Choix du Thèmes</h1>
+        <br>
+        <div class="col"> <!-- Ajout de la classe 'col-md-8' -->
+            <p class="responsive-text">Thèmes actuellement sélectionnée : </p>
+            <div class="dropdown">
+                <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                    Thèmes
+                </button>
+                <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+                    <a class="dropdown-item" href="#">Clair</a>
+                    <a class="dropdown-item" href="#">Sombre</a>
+                </div>
+
+            </div>
+        </div>
+    </div>
+    </body>
