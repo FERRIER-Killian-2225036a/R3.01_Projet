e1813347f9be39da2a32d5bd2521d75bab5b7d36
Killian Ferrier <killian.ferrier@etu.univ-amu.fr>
2023-10-26 12:02:27 +0200

    Tests pour régler bugs

--- common_scripts/showErrorMessage.js
+++ common_scripts/showErrorMessage.js
@@ -1,11 +1,12 @@
 function ShowLoginErrorMessage(status) {
     const errorMessage = document.getElementById('wrongInfo');
+    errorMessage.style.display = 'inherit';
     let errorMessageText = errorMessage[0].textContent;
     console.log(status, errorMessageText);
     if (status!=="success") {
         errorMessage.style.display = 'inherit';
     } else {
-        errorMessage[0].innerHTML = status;
+        errorMessageText.innerHTML = status;
         errorMessage.style.display = 'none';
     }
 }
