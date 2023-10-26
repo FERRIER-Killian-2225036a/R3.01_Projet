1cb7413b08e30a34404dfe9c596eb24fda263a43
Killian Ferrier <killian.ferrier@etu.univ-amu.fr>
2023-10-26 22:59:06 +0200

    Réparation bétise

--- views/main.php
+++ views/main.php
@@ -14,8 +14,7 @@
     <body>
         <?php
             motorView::show('common/header');
-            //echo $mapView['body'];
-            motorView::show('post/viewBlogEdit');
+            echo $mapView['body'];
             motorView::show('common/help');
             motorView::show('common/footer');
             require 'common_scripts/general_scripts.php';
