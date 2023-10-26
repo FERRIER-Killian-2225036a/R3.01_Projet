53b81d4422264f40b976f1ee8895ef12ff314014
Pierrick Caron <pierrick.caron@etu.univ-amu.fr>
2023-10-26 16:05:31 +0200

    Début de la page des commentaires

--- views/profileSettings/myComment.php
+++ views/profileSettings/myComment.php
@@ -1,1 +1,44 @@
-<?php
+<?php require 'sideBar.php'; ?>
+<div class="col container">
+    <!-- Section pour le titre -->
+    <div class="row mb-2 mt-2">
+        <div class="col-md-5">
+            <h1>Vos Commentaires </h1>
+        </div>
+        <!-- Section pour le champs de recherche -->
+        <div class="col-md-6 d-flex align-items-center">
+            <input class="form-control" type="text" name="query" placeholder="Rechercher...">
+        </div>
+        <hr class="my-3">
+        <!-- Section pour la table -->
+        <table class="table table-striped">
+            <thead>
+            <tr>
+                <th scope="col">#</th>
+                <th scope="col">Titre</th>
+                <th scope="col">Status</th>
+                <th scope="col">Dates</th>
+            </tr>
+            </thead>
+            <tbody>
+            <tr>
+                <th scope="row">1</th>
+                <td>CHAPI CHAPO QUESTION ( blablabla les premieres lignes )
+                <br><small> -> vous à vous bonjour</small></td>
+                <td>Nouveau</td>
+                <td>2022-03-17</td>
+            </tr>
+            <tr>
+                <th scope="row">2</th>
+                <td>CHAPI CHAPO QUESTION ( blablabla les premieres lignes )</td>
+                <td>Ancien</td>
+                <td>2022-03-17</td>
+            </tr>
+            <tr>
+                <th scope="row">3</th>
+                <td>CHAPI CHAPO QUESTION ( blablabla les premieres lignes )</td>
+                <td>Ancien</td>
+                <td>2022-03-17</td>
+            </tr>
+            </tbody>
+        </table>
