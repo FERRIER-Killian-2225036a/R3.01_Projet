a173b64969576b0bdf3cc20e0bb49e88dd6e51f4
Tom Carvajal <tom.carvajal@etu.univ-amu.fr>
2023-10-27 20:57:38 +0200

    test affichage bouton selon page

--- views/common/header.php
+++ views/common/header.php
@@ -1,65 +1,69 @@
 <body>
-    <nav class="navbar navbar-top navbar-expand-lg navbar-light bg-light">
-        <div class="container-fluid">
-            <a class="navbar-brand" href="/">
-                <img src="/media/public_assets/CyphubLogo.png" alt="Logo Cyphub" id="logoImg" class="d-inline-block align-text-top">
-            </a>
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-            </button>
-            <div class="collapse navbar-collapse justify-content-end" id="navbarSupportedContent">
-                <ul class="navbar-nav responsive-menu ml-auto mb-2 mb-lg-0">
-                    <li class="nav-item">
-                        <form class="d-flex">
-                            <input class="form-control" type="search" placeholder="Search" aria-label="Search">
-                        </form>
-                    </li>
-                    <li class="nav-item">
-                        <a href="<?= SessionManager::isUserConnected() ? "/Settings/ManageAccount" : "/Auth/Login"; ?>">
-                            <img src="<?= (SessionManager::isUserConnected() && $_SESSION['UrlPicture']!==null ) ? $_SESSION['UrlPicture'] : Constants::PDP_URL_DEFAULT; ?>" alt="Logo profil" class="menu-img">
-                        </a>
-                    </li>
-                    <li class="nav-item">
-                        <a href="/Auth/LogOut">
-                            <img src="/media/public_assets/logout.png" alt="Logo logout" class="menu-img me-2">
-                        </a>
-                    </li>
-                </ul>
+<nav class="navbar navbar-top navbar-expand-lg navbar-light bg-light">
+    <div class="container-fluid">
+        <a class="navbar-brand" href="/">
+            <img src="/media/public_assets/CyphubLogo.png" alt="Logo Cyphub" id="logoImg"
+                 class="d-inline-block align-text-top">
+        </a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent"
+                aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse justify-content-end" id="navbarSupportedContent">
+            <ul class="navbar-nav responsive-menu ml-auto mb-2 mb-lg-0">
+                <li class="nav-item">
+                    <form class="d-flex">
+                        <input class="form-control" type="search" placeholder="Search" aria-label="Search">
+                    </form>
+                </li>
+                <li class="nav-item">
+                    <a href="<?= SessionManager::isUserConnected() ? "/Settings/ManageAccount" : "/Auth/Login"; ?>">
+                        <img src="<?= (SessionManager::isUserConnected() && $_SESSION['UrlPicture'] !== null) ? $_SESSION['UrlPicture'] : Constants::PDP_URL_DEFAULT; ?>"
+                             alt="Logo profil" class="menu-img">
+                    </a>
+                </li>
+                <li class="nav-item">
+                    <a href="/Auth/LogOut">
+                        <img src="/media/public_assets/logout.png" alt="Logo logout" class="menu-img me-2">
+                    </a>
+                </li>
+            </ul>
 
-            </div>
         </div>
-    </nav>
+    </div>
+</nav>
 
 
-    <nav class="navbar navbar-bottom navbar-expand navbar-light bg-light">
-        <div class="container-fluid">
+<nav class="navbar navbar-bottom navbar-expand navbar-light bg-light">
+    <div class="container-fluid">
 
-            <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
-                <div class="navbar-nav">
-                    <a class="nav-link" aria-current="page" href="/">Home</a>
-                    <a class="nav-link" href="/Menu/ActualityFeed">Veille Tech</a>
-                    <a class="nav-link" href="/Menu/BlogFeed">Blogs</a>
-                    <!--<a class="nav-link" href="/Menu/ForumFeed">Forum</a>-->
-                    <?php //si c'est bon on affiche
+        <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+            <div class="navbar-nav">
+                <a class="nav-link" aria-current="page" href="/">Home</a>
+                <a class="nav-link" href="/Menu/ActualityFeed">Veille Tech</a>
+                <a class="nav-link" href="/Menu/BlogFeed">Blogs</a>
+                <!--<a class="nav-link" href="/Menu/ForumFeed">Forum</a>-->
+                <?php //si c'est bon on affiche
                     if (SessionManager::isUserConnected()) {
-                        //si la page est /Menu/BlogFeed
-                        if ( $this->_mapOfSplitUrl['controller'] === 'ControllerMenu' && $this->_mapOfSplitUrl['action'] === 'BlogFeedAction') {
+                        //si on est connecté, et dans la page /Menu/BlogFeed $leliendelarequete = "/Post/BlogEdit";
+                        // et $nomTypePost = "Blog"
+                        // si on est connecté et dans la page /Menu/ForumFeed $leliendelarequete = "/Post/ForumEdit";
+                        // et $nomTypePost = "Forum"
+                        if ($_SERVER['REQUEST_URI'] === "/Post/BlogEdit") {
                             $leliendelarequete = "/Post/BlogEdit";
                             $nomTypePost = "Blog";
-                        } else if ($this->_mapOfSplitUrl['controller'] === 'ControllerMenu' && $this->_mapOfSplitUrl['action'] === 'ForumFeedAction') {
+                            echo '<a class="NewPost" href="'.$leliendelarequete.'">Nouveau' .$nomTypePost.' </a>';
+
+                        } elseif ($_SERVER['REQUEST_URI'] === "/Post/ForumEdit") {
                             $leliendelarequete = "/Post/ForumEdit";
                             $nomTypePost = "Forum";
+                            echo '<a class="NewPost" href="'.$leliendelarequete.'">Nouveau' .$nomTypePost.' </a>';
+
                         }
-                    }
-                     //si on est connecté, et dans la page /Menu/BlogFeed $leliendelarequete = "/Post/BlogEdit";
-                    // et $nomTypePost = "Blog"
-                    // si on est connecté et dans la page /Menu/ForumFeed $leliendelarequete = "/Post/ForumEdit";
-                    // et $nomTypePost = "Forum"
-                    echo '<a class="NewPost" href="'.$leliendelarequete.'">Nouveau' .$nomTypePost.' </a>'
-                    ?>
-                </div>
+                    }?>
             </div>
         </div>
-    </nav>
-    <script src="../../common_scripts/navLinks.js"></script>
+    </div>
+</nav>
+<script src="../../common_scripts/navLinks.js"></script>
 </body>
