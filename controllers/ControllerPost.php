5b3a75e37d4aa5801a28b50ac80677bb93f80983
Tom Carvajal <tom.carvajal@etu.univ-amu.fr>
2023-10-29 17:43:59 +0100

    Premier debug edition de blog

--- controllers/ControllerPost.php
+++ controllers/ControllerPost.php
@@ -2,10 +2,19 @@
 
 class ControllerPost
 {
-    public function BlogEditAction(): void
+    public function BlogEditAction(array $A_parametres = null, array $A_postParams = null): void
     {
+
+        print_r($A_parametres);
+        print_r($A_postParams);
+
+
+
+
         MotorView::show('post/viewBlogEdit');
         // si la requete est, post/blogEdit sans rien derriere on a a faire a la création d'un nouveau post
+
+
 
         // si la requete contient un identifiant on verifie qu'il est valide et cohérent,
             // on passe a la modification
