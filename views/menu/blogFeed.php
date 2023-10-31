df336772fb1e4c15981a942e5c11a98d00c195ff
Pierrick Caron <pierrick.caron@etu.univ-amu.fr>
2023-10-31 13:46:21 +0100

    Réparation de la page blogFeed.php

--- views/menu/blogFeed.php
+++ views/menu/blogFeed.php
@@ -1,31 +1,29 @@
 <link rel="stylesheet" href="../../common_styles/menu.css">
-<main class="container p-5">
+<link rel="stylesheet" href="../../common_styles/general.css">
+<main class="container">
     <div class="p-3">
         <div class="btn bg-body-tertiary round background grow-button" role="button">
-            <a href="<?php echo $mapView['blogPostUrl'] ?>">
-                <img src="<?php echo $mapView['blogUrlPicture'] ?>" alt="Logo" class="responsive-image round p-1">
-                <div class="text-content">
-                    <h1 class="responsiveTitle"><?php echo $mapView['blogTitle'] ?></h1>
-                    <p class="lead"><?php echo $mapView['Tags'] ?> <?php echo $mapView['blogAuthor'] ?> - <?php echo $mapView['blogDate'] ?> </p>
-                    <p class="responsive-text"><?php echo $mapView['blogContent'] ?></p>
-                </div>
-            </a>
-            <div class="col-12 col-md-6 col-lg-4">
-                <div class="input-group align-items-center">
-                    <label>
-                        <input type="text" class="form-control custom-input inputBackground" placeholder="Commenter">
-                    </label>
-                    <div class="input-group-append">
-                        <button> <!--type="submit"-->
-                            <!-- TODO : mettre le bon lien formulaire requete + input commenter-->
-                            <img src="../../media/public_assets/icone/signet.png"
-                                 alt="Icon de signet pour mettre en favorie" id="signetImg">
-                        </button>
+            <img src="../../media/public_assets/imageTest.jpeg" alt="Logo" class="responsive-image round p-1">
+            <div class="text-content">
+                <h1 class="responsiveTitle">Test du blog pour que tout fonctionne bien</h1>
+                <p class="lead responsiveText"> Catégorie - 00-00-00 - Author </p>
+                <p class="responsiveText">Je sais pas Je sais pasJe sais pas Je sais pas Je sais pas Je sais pas Je sais pas Je sais pasJe sais pas </p>
+                <!-- Début de la section commentaire -->
+                <div class="row">
+                    <div class="col-12 col-md-6 col-lg-4">
+                        <div class="input-group align-items-center">
+                            <input type="text" class="form-control custom-input inputBackground" placeholder="Commenter">
+                            <div class="input-group-append">
+                                <button class="btn btn-outline-secondary" type="button">
+                                    <i class="fas fa-save">#blog</i>
+                                </button>
+                            </div>
+                        </div>
                     </div>
                 </div>
+                <!-- Fin de la section commentaire -->
             </div>
         </div>
-
     </div>
     <script src="../../common_scripts/maxTextSize.js"></script>
 </main>
