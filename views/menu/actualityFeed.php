09fc7f3eda2d83d87b1049b69f98561b1d2a700d
Pierrick Caron <pierrick.caron@etu.univ-amu.fr>
2023-10-31 14:01:38 +0100

    Modification de certaines class dans actualityFeed.php

--- views/menu/actualityFeed.php
+++ views/menu/actualityFeed.php
@@ -1,12 +1,13 @@
 <link rel="stylesheet" href="../../common_styles/menu.css">
+<link rel="stylesheet" href="../../common_styles/general.css">
 <main class="container">
     <div class="p-3">
         <a class="btn bg-body-tertiary round background grow-button" role="button" href="<?php echo $mapView['actualityLien'] ?>" target="_blank">
             <img src="<?php echo $mapView['actualityUrlPicture'] ?>" alt="Logo" class="responsive-image round p-1">
             <div class="text-content">
-                <h1 class="responsiveTitle"><?php echo $mapView['actualityTitle']; ?></h1>
-                <p class="lead responsiveText"> Catégorie - <?php echo $mapView['actualityDate']; ?> - <?php echo $mapView['actualityAuthor']; ?> </p> <!-- TODO : catégorie -->
-                <p class="responsiveText"><?php echo $mapView['actualityContent']; ?></p>
+                <h1 class="responsive-title"><?php echo $mapView['actualityTitle']; ?></h1>
+                <p class="lead responsive-text"> Catégorie - <?php echo $mapView['actualityDate']; ?> - <?php echo $mapView['actualityAuthor']; ?> </p> <!-- TODO : catégorie -->
+                <p class="responsive-text"><?php echo $mapView['actualityContent']; ?></p>
             </div>
         </a>
     </div>
