7f5ae46a655ec02f89f075b2fff86222c44d3944
Pierrick Caron <pierrick.caron@etu.univ-amu.fr>
2023-10-31 14:04:37 +0100

    Version corrigé du sript pour que tout fonctionne

--- common_scripts/maxTextSize.js
+++ common_scripts/maxTextSize.js
@@ -1,18 +1,23 @@
 maxTextLength = 260;
 maxTitleLength = 35;
+
 function maxTextSizeForContent() {
     let classTextResponsive = document.getElementsByClassName("responsive-text");
-    let text = classTextResponsive[0].textContent;
-    if (text.length > maxTextLength) {
-        classTextResponsive[0].innerHTML = text.slice(0, maxTextLength) + " <b>See more...</b>";
+    for (let i = 0; i < classTextResponsive.length; i++) {
+        let text = classTextResponsive[i].textContent;
+        if (text.length > maxTextLength) {
+            classTextResponsive[i].innerHTML = text.slice(0, maxTextLength) + " <b>See more...</b>";
+        }
     }
 }
 
 function maxTextSizeForTitle() {
-    let classTitleResponsive = document.getElementsByClassName("responsiveTitle");
-    let text = classTitleResponsive[0].textContent;
-    if (text.length > maxTitleLength) {
-        classTitleResponsive[0].innerHTML = text.slice(0, maxTitleLength) + "...";
+    let classTitleResponsive = document.getElementsByClassName("responsive-title");
+    for (let i = 0; i < classTitleResponsive.length; i++) {
+        let text = classTitleResponsive[i].textContent;
+        if (text.length > maxTitleLength) {
+            classTitleResponsive[i].innerHTML = text.slice(0, maxTitleLength) + "...";
+        }
     }
 }
 
