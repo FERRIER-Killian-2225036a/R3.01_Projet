bda2e70daf8d65997f51089fd11ddc02e61d2041
Pierrick Caron <pierrick.caron@etu.univ-amu.fr>
2023-10-31 15:02:15 +0100

    Page viewBlog.php fini

--- views/post/viewBlog.php
+++ views/post/viewBlog.php
@@ -1,1 +1,38 @@
-<?php
+<div class="container">
+    <div class="text-left mt-4">
+        <h1>Titre du Blog</h1>
+        <div class="row mb-4">
+            <div class="col-md-6 d-flex align-items-center">
+                <img src="../../media/public_assets/favicon.png" alt="image" class="rounded-circle mr-3">
+                <div>
+                    <p class="mb-0">KrokmouLeFurynocturne <a class="btn btn-custom-purple">Suivre</a></p>
+                    <small>99 Abonnés     99 Abonnements     1K Posts</small>
+                </div>
+            </div>
+            <div class="col-md-6 d-flex align-items-center justify-content-end">
+                <a href="#" title="Partager">
+                    <img src="../../media/public_assets/icone/partager.png" alt="Partager" class="icon">
+                </a>
+                <a href="#" title="Signet">
+                    <img src="../../media/public_assets/icone/signet.png" alt="Signet" class="icon">
+                </a>
+            </div>
+        </div>
+        <hr>
+
+        <div class="row justify-content-center mt-4">
+            <div class="col-8">
+                <img src="../../media/public_assets/imageTest.jpeg" class="img-fluid" alt="Image du Blog">
+            </div>
+        </div>
+
+        <div class="row justify-content-center mt-4">
+            <div class="col-8">
+                <p>
+                    Texte de votre blog. Vous pouvez ajouter ici toutes les informations que vous souhaitez partager dans votre blog.
+                    Assurez-vous de personnaliser ce texte en fonction de votre contenu.
+                </p>
+            </div>
+        </div>
+    </div>
+</div>
