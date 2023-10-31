eacd177b4f3b1fd69fa51b040bed5316193e4bf1
Pierrick Caron <pierrick.caron@etu.univ-amu.fr>
2023-10-31 13:54:29 +0100

    Réparation de la page myPost.php

--- views/profileSettings/myPost.php
+++ views/profileSettings/myPost.php
@@ -30,5 +30,28 @@
         </div>
         <hr class="my-3">
         <!--Section pour les articles enregistrer -->
-
+        <div class="col-md-6 p-3">
+            <div class="btn bg-body-tertiary round background grow-button d-block" role="button">
+                <div class="d-flex justify-content-center mb-2">
+                    <img src="../../media/public_assets/imageTest.jpeg" alt="Logo" class="responsive-image-setting round p-1">
+                </div>
+                <div class="text-content">
+                    <h1 class="responsiveTitle">Test de myPost</h1>
+                    <p class="lead responsiveText">Catégorie - 00-00-00 - Par l'auteur</p>
+                    <p class="responsiveText">ça va parler chinois : Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit neskwik... VOIR PLUS</p>
+                </div>
+            </div>
+        </div>
+        <div class="col-md-6 p-3">
+            <div class="btn bg-body-tertiary round background grow-button d-block" role="button">
+                <div class="d-flex justify-content-center mb-2">
+                    <img src="../../media/public_assets/imageTest.jpeg" alt="Logo" class="responsive-image-setting round p-1">
+                </div>
+                <div class="text-content">
+                    <h1 class="responsiveTitle">Test de myPost</h1>
+                    <p class="lead responsiveText">Catégorie - 00-00-00 - Par l'auteur</p>
+                    <p class="responsiveText">ça va parler chinois : Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit neskwik... VOIR PLUS</p>
+                </div>
+            </div>
+        </div>
 
