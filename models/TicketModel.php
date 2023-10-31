899db7a5ad0f825d70e9fa28b91384783e1242eb
Tom Carvajal <tom.carvajal@etu.univ-amu.fr>
2023-10-31 18:29:59 +0100

    debug

--- models/TicketModel.php
+++ models/TicketModel.php
@@ -50,6 +50,7 @@ class TicketModel
     {
         $tempObj = new Ticket();
         $arrayOfValues = $tempObj->getValuesByUserId($UserId);
+        error_log("array of values in ticket model = ".print_r($arrayOfValues, true));
         $this->TicketId = $arrayOfValues['TicketId'];
         $this->mail = $arrayOfValues['mail'];
         $this->date = $arrayOfValues['date'];
