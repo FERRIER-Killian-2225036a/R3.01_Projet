631adf93fbd4c1e49a593c3b093324618b88f4ab
Pierrick Caron <pierrick.caron@etu.univ-amu.fr>
2023-10-31 14:33:34 +0100

    Placement du bouton

--- views/common/header.php
+++ views/common/header.php
@@ -2,20 +2,20 @@
 <nav class="navbar navbar-top navbar-expand-lg navbar-light bg-light">
     <div class="container-fluid">
         <a class="navbar-brand" href="/">
-            <img src="/media/public_assets/CyphubLogo.png" alt="Logo Cyphub" id="logoImg"
-                 class="d-inline-block align-text-top">
+            <img src="/media/public_assets/CyphubLogo.png" alt="Logo Cyphub" id="logoImg" class="d-inline-block align-text-top">
         </a>
-        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent"
-                aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
-        <div class="collapse navbar-collapse justify-content-end" id="navbarSupportedContent">
-            <ul class="navbar-nav responsive-menu ml-auto mb-2 mb-lg-0">
+        <div class="collapse navbar-collapse justify-content-between" id="navbarSupportedContent">
+            <ul class="navbar-nav responsive-menu">
                 <li class="nav-item">
                     <form class="d-flex">
                         <input class="form-control" type="search" placeholder="Search" aria-label="Search">
                     </form>
                 </li>
+            </ul>
+            <ul class="navbar-nav responsive-menu">
                 <li class="nav-item">
                     <a href="<?= SessionManager::isUserConnected() ? "/Settings/ManageAccount" : "/Auth/Login"; ?>">
                         <img src="<?= (SessionManager::isUserConnected() && $_SESSION['UrlPicture'] !== null) ? $_SESSION['UrlPicture'] : Constants::PDP_URL_DEFAULT; ?>"
@@ -36,13 +36,12 @@
     <div class="container-fluid">
         <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
             <div class="navbar-nav">
-                <a class="nav-link" aria-current="page" href="/">Home</a>
-                <a class="nav-link" href="/Menu/ActualityFeed">Veille Tech</a>
-                <a class="nav-link" href="/Menu/BlogFeed">Blogs</a>
-                <!--
-                <a class="nav-link" href="/Menu/ForumFeed">Forum</a>
-                -->
-                <?php // Si l'utilisateur est connecté et sur la page Blog, vous pouvez générer des liens spécifiques ici
+                <div class="d-flex justify-content-between">
+                    <a class="nav-link" aria-current="page" href="/">Home</a>
+                    <a class="nav-link" href="/Menu/ActualityFeed">Veille Tech</a>
+                    <a class="nav-link" href="/Menu/BlogFeed">Blogs</a>
+                </div>
+                <?php
                 if (SessionManager::isUserConnected() && $_SERVER['REQUEST_URI'] === "/Menu/BlogFeed") {
                     echo '<a class="btn btn-outline-dark ms-auto" href="/Post/BlogEdit">Nouveau Blog</a>';
                 }
