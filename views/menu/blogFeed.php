76029441547d3cf85c632f4e0196e5d6eeb38f38
Killian Ferrier <killian.ferrier@etu.univ-amu.fr>
2023-11-01 19:56:09 +0100

    Réparation de suppréssion

--- views/menu/blogFeed.php
+++ views/menu/blogFeed.php
@@ -5,14 +5,16 @@
         <div class="btn bg-body-tertiary round background grow-button" role="button">
             <img src="../../media/public_assets/imageTest.jpeg" alt="Logo" class="responsive-image round p-1">
             <div class="text-content">
-                <h1 class="responsiveTitle">Test du blog pour que tout fonctionne bien</h1>
-                <p class="lead responsiveText"> Catégorie - 00-00-00 - Author </p>
-                <p class="responsiveText">Je sais pas Je sais pasJe sais pas Je sais pas Je sais pas Je sais pas Je sais pas Je sais pasJe sais pas </p>
+                <h1 class="responsiveTitle"><?php echo $mapView['blogTitle'] ?></h1>
+                <p class="lead responsiveText"><?php echo $mapView['Tags'] ?> <?php echo $mapView['blogAuthor'] ?> - <?php echo $mapView['blogDate'] ?> </p>
+                <p class="responsiveText"><?php echo $mapView['blogContent'] ?></p>
                 <!-- Début de la section commentaire -->
                 <div class="row">
                     <div class="col-12 col-md-6 col-lg-4">
                         <div class="input-group align-items-center">
-                            <input type="text" class="form-control custom-input inputBackground" placeholder="Commenter">
+                            <label>
+                                <input type="text" class="form-control custom-input inputBackground" placeholder="Commenter">
+                            </label>
                             <div class="input-group-append">
                                 <button class="btn btn-outline-secondary" type="button">
                                     <i class="fas fa-save">#blog</i>
