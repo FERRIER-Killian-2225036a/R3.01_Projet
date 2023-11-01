84bea7803406a1b1a13e2ee12730848691153d94
Killian Ferrier <killian.ferrier@etu.univ-amu.fr>
2023-11-02 00:52:09 +0100

    Ajustement de la localisation dans la sidebar

--- common_scripts/locationInSidebar.js
+++ common_scripts/locationInSidebar.js
@@ -1,11 +1,8 @@
-// Récupérez le chemin de l'URL actuelle
-let currentPath = window.location.pathname;
-
 // Récupérez tous les éléments de navigation (liens)
-const navLinks = document.querySelectorAll('.nav-link');
+const navLinksSidebar = document.querySelectorAll('.nav-link');
 
 // Parcourez les liens
-navLinks.forEach(link => {
+navLinksSidebar.forEach(link => {
     // Vérifiez si le chemin de l'URL correspond à l'attribut "href" du lien
     if (currentPath === link.getAttribute('href')) {
         // Ajoutez la classe "active" au lien si le chemin correspond
