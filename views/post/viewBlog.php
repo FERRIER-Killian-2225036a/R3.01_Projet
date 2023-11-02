38f1daa89bf01bef7b22e68973854341f1b8ff05
Killian Ferrier <killian.ferrier@etu.univ-amu.fr>
2023-11-02 23:44:12 +0100

    Ajout des élément php dans les views

--- views/post/viewBlog.php
+++ views/post/viewBlog.php
@@ -1,12 +1,12 @@
 <div class="container">
     <div class="text-left mt-4">
-        <h1><?php echo $mapView['title'] ?></h1>
+        <h1><?php echo $mapView['Title'] ?></h1>
         <div class="row mb-4">
             <div class="col-md-6 d-flex align-items-center">
-                <img src="<?php echo $mapView['$imgProfil'] ?>" alt="image" class="rounded-circle mr-3">
+                <img src="<?php echo $mapView['$ImgProfil'] ?>" alt="image" class="rounded-circle mr-3">
                 <div>
-                    <p class="mb-0"><?php echo $mapView['author'] ?> <a class="btn btn-custom-purple">Suivre</a></p>
-                    <small><?php echo $mapView['numberOfFollower'] ?></small>
+                    <p class="mb-0"><?php echo $mapView['Author'] ?> <a class="btn btn-custom-purple">Suivre</a></p>
+                    <small><?php echo $mapView['NumberOfFollower'] ?></small>
                 </div>
             </div>
             <div class="col-md-6 d-flex align-items-center justify-content-end">
@@ -22,14 +22,14 @@
 
         <div class="row justify-content-center mt-4">
             <div class="col-8">
-                <img src="<?php echo $mapView['img'] ?>" class="img-fluid" alt="Image du Blog">
+                <img src="<?php echo $mapView['Img'] ?>" class="img-fluid" alt="Image du Blog">
             </div>
         </div>
 
         <div class="row justify-content-center mt-4">
             <div class="col-8">
                 <p>
-                    <?php echo $mapView['content'] ?>
+                    <?php echo $mapView['Content'] ?>
                 </p>
             </div>
         </div>
