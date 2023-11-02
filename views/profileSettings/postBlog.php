accc9323ab7523acff89c56f9da9f5c1244019c5
Killian Ferrier <killian.ferrier@etu.univ-amu.fr>
2023-11-02 22:03:48 +0100

    Test résolution bug bouton ...

--- views/profileSettings/postBlog.php
+++ views/profileSettings/postBlog.php
@@ -4,24 +4,24 @@
             <img src="<?php echo $mapView['blogUrlPicture'] ?>" alt="Logo" class="responsive-image-setting round p-1">
         </div>
         <div class="text-content">
-                    <span class="d-flex">
-                        <h1 class="responsive-title"><?php echo $mapView['blogTitle'] ?></h1>
-                        <div class="dropdown">
-                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
-                                 class="bi bi-three-dots" viewBox="0 0 16 16">
-                                <path d="M3 9.5a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3zm5 0a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3zm5 0a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3z"/>
-                            </svg>
-                            <div class="dropdown-content">
-                                <a href="<?php echo $mapView['blogPostEditUrl'] ?>">Modifier</a>
-                                <a id="modifyVisibilityButton" > <?php if ($mapView['statusP'] != "innactive") {
-                                        echo ($mapView['statusP'] == "active") ? "Mettre en visibilité privé" : "Mettre en visibilité public";
-                                    } ?></a>
-                                <!-- De toute facon on affiche pas la publication si innactive + test dans le controller pour la requete post-->
+            <span class="d-flex">
+                <h1 class="responsive-title"><?php echo $mapView['blogTitle'] ?></h1>
+                <div class="dropdown">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
+                         class="bi bi-three-dots" viewBox="0 0 16 16">
+                        <path d="M3 9.5a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3zm5 0a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3zm5 0a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3z"/>
+                    </svg>
+                    <div class="dropdown-content">
+                        <a href="<?php echo $mapView['blogPostEditUrl'] ?>">Modifier</a>
+                        <a id="modifyVisibilityButton" > <?php if ($mapView['statusP'] != "innactive") {
+                                echo ($mapView['statusP'] == "active") ? "Mettre en visibilité privé" : "Mettre en visibilité public";
+                            } ?></a>
+                        <!-- De toute facon on affiche pas la publication si innactive + test dans le controller pour la requete post-->
 
-                                <a id="deleteButton" >Supprimer</a>
-                            </div>
-                        </div>
-                    </span>
+                        <a id="deleteButton" >Supprimer</a>
+                    </div>
+                </div>
+            </span>
             <p class="lead responsive-text"><?php echo $mapView['blogTags'] ?> <?php echo $mapView['blogDate'] ?>
                 - Par <?php echo $mapView['blogAuthor'] ?></p>
             <p class="responsive-text"><?php echo $mapView['blogContent'] ?></p>
@@ -54,7 +54,13 @@
     });
 
     document.getElementById('divButtonRole').addEventListener("click", function() {
-        window.location.href = "<?php echo $mapView['blogPostEditUrl'] ?>";
+        //const isInput = event.target.closest('.input-group');
+        const dropdown = event.target.classList.contains('dropdown');
+        //!isInput &&
+        if ( !dropdown) {
+            window.location.href = "<?php echo $mapView['blogPostEditUrl'] ?>";
+        }
+
     });
 </script>
 </div>
