5a1f1cb98cc6415caa6e8bf900178f64ab42f956
Killian Ferrier <killian.ferrier@etu.univ-amu.fr>
2023-11-02 16:03:37 +0100

    déclaration du js après code

--- views/profileSettings/myPost.php
+++ views/profileSettings/myPost.php
@@ -1,6 +1,5 @@
 <?php require 'sideBar.php' ?>
 <script src="/common_scripts/dropdown.js"></script>
-<script src="/common_scripts/myPostDisplay.js"></script>
 <link rel="stylesheet" href="/common_styles/dropdown.css">
 
 <div class="col container" id="rightSide">
@@ -39,22 +38,22 @@
                 </div>
                 <div class="text-content">
                     <span class="d-flex">
-                    <h1 class="responsive-title"><?php echo $mapView['blogTitle'] ?></h1>
-                    <div class="dropdown">
-                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
-                             class="bi bi-three-dots" viewBox="0 0 16 16">
-                            <path d="M3 9.5a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3zm5 0a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3zm5 0a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3z"/>
-                        </svg>
-                        <div class="dropdown-content">
-                            <a href="<?php echo $mapView['blogPostEditUrl'] ?>">Modifier</a>
-                            <a onclick="sendPostShowOrHide('<?php echo $mapView['id'] ?>')"> <?php if ($mapView['statusP'] != "innactive") {
-                                    echo ($mapView['statusP'] == "active") ? "Mettre en visibilité public" : "Mettre en visibilité privé";
-                                } ?></a>
-                            <!-- De toute facon on affiche pas la publication si innactive + test dans le controller pour la requete post-->
+                        <h1 class="responsive-title"><?php echo $mapView['blogTitle'] ?></h1>
+                        <div class="dropdown">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
+                                 class="bi bi-three-dots" viewBox="0 0 16 16">
+                                <path d="M3 9.5a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3zm5 0a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3zm5 0a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3z"/>
+                            </svg>
+                            <div class="dropdown-content">
+                                <a href="<?php echo $mapView['blogPostEditUrl'] ?>">Modifier</a>
+                                <a onclick="sendPostShowOrHide('<?php echo $mapView['id'] ?>')"> <?php if ($mapView['statusP'] != "innactive") {
+                                        echo ($mapView['statusP'] == "active") ? "Mettre en visibilité public" : "Mettre en visibilité privé";
+                                    } ?></a>
+                                <!-- De toute facon on affiche pas la publication si innactive + test dans le controller pour la requete post-->
 
-                            <a onclick="sendPostDelete('<?php echo $mapView['id'] ?>')">Supprimer</a>
+                                <a onclick="sendPostDelete('<?php echo $mapView['id'] ?>')">Supprimer</a>
+                            </div>
                         </div>
-                    </div>
                     </span>
                     <p class="lead responsive-text"><?php echo $mapView['blogTags'] ?> <?php echo $mapView['blogDate'] ?>
                         - Par <?php echo $mapView['blogAuthor'] ?></p>
@@ -64,6 +63,7 @@
         </div>
         </div>
     </div>
+    <script src="/common_scripts/myPostDisplay.js"></script>
     <script src="/common_scripts/maxTextSize.js"></script>
     <script src="/common_scripts/postOptions.js"></script>
 
