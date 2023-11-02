2e3ae84e11156b1d121a77c2b2383011f56c79e0
Killian Ferrier <killian.ferrier@etu.univ-amu.fr>
2023-11-02 12:11:50 +0100

    Test post vide

--- views/profileSettings/myPost.php
+++ views/profileSettings/myPost.php
@@ -31,62 +31,58 @@
         <hr class="my-3">
         <!--Section pour les articles enregistrer -->
         <div class="col-md-6 p-3">
-            <div class="btn bg-body-tertiary round background grow-button d-block" role="button">
+
+            <a class="btn bg-body-tertiary round background grow-button d-block" role="button"
+               href="<?php echo $mapView['blogPostEditUrl'] ?>"
+               target="_blank">
                 <div class="d-flex justify-content-center mb-2">
-                    <img src="../../media/public_assets/imageTest.jpeg" alt="Logo" class="responsive-image-setting round p-1">
+                    <img src="<?php echo $mapView['blogUrlPicture'] ?>" alt="Logo"
+                         class="responsive-image-setting round p-1">
                 </div>
                 <div class="text-content">
-                    <span class="d-flex">
-                    <h1 class="responsive-title">Test de myPost avec un titre grand pour vérifier que le js marche bien sinon ca serait très decevant quand mm tu ne trouves pas ?? moi je trouve que si après je parle tout seul donc bon c'est triste</h1>
-                    <div class="dropdown">
-                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
-                             class="bi bi-three-dots" viewBox="0 0 16 16">
-                            <path d="M3 9.5a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3zm5 0a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3zm5 0a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3z"/>
-                        </svg>
-                        <div class="dropdown-content">
-                            <a href="<?php echo $mapView['blogPostEditUrl'] ?>">Modifier</a>
-                            <a onclick="sendPostShowOrHide('<?php echo $mapView['id'] ?>')"> <?php if ($mapView['statusP'] != "innactive") {
-                                    echo ($mapView['statusP'] == "active") ? "Mettre en visibilité public" : "Mettre en visibilité privé";
-                                } ?></a>
-                            <!-- De toute facon on affiche pas la publication si innactive + test dans le controller pour la requete post-->
+                    <h1 class="responsiveTitle"><?php echo $mapView['blogTitle'] ?></h1>
+                    <p class="lead responsiveText"><?php echo $mapView['blogTags'] ?> <?php echo $mapView['blogDate'] ?>
+                        - Par <?php echo $mapView['blogAuthor'] ?></p>
+                    <p class="responsiveText"><?php echo $mapView['blogContent'] ?></p> <!-- ajouté categories-->
+                </div>
+            </a>
+            <div class="dropdown">
+                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
+                     class="bi bi-three-dots" viewBox="0 0 16 16">
+                    <path d="M3 9.5a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3zm5 0a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3zm5 0a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3z"/>
+                </svg>
+                <div class="dropdown-content">
+                    <a href="<?php echo $mapView['blogPostEditUrl'] ?>">Modifier</a>
+                    <a onclick="sendPostShowOrHide('<?php echo $mapView['id'] ?>')"> <?php if ($mapView['statusP'] != "innactive") {
+                            echo ($mapView['statusP'] == "active") ? "Mettre en visibilité public" : "Mettre en visibilité privé";
+                        } ?></a>
+                    <!-- De toute facon on affiche pas la publication si innactive + test dans le controller pour la requete post-->
 
-                            <a onclick="sendPostDelete('<?php echo $mapView['id'] ?>')">Supprimer</a>
-                        </div>
-                    </div>
-                    </span>
-                    <p class="lead responsive-text">Catégorie - 00-00-00 - Par l'auteur</p>
-                    <p class="responsive-text">ça va parler chinois : Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit neskwik</p>
+                    <a onclick="sendPostDelete('<?php echo $mapView['id'] ?>')">Supprimer</a>
                 </div>
             </div>
-        </div>
-        <div class="col-md-6 p-3">
-            <div class="btn bg-body-tertiary round background grow-button d-block" role="button">
-                <div class="d-flex justify-content-center mb-2">
-                    <img src="../../media/public_assets/imageTest.jpeg" alt="Logo" class="responsive-image-setting round p-1">
-                </div>
-                <div class="text-content">
-                    <span class="d-flex">
-                    <h1 class="responsive-title">Test de myPost</h1>
-                    <div class="dropdown">
-                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
-                             class="bi bi-three-dots" viewBox="0 0 16 16">
-                            <path d="M3 9.5a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3zm5 0a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3zm5 0a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3z"/>
-                        </svg>
-                        <div class="dropdown-content">
-                            <a href="<?php echo $mapView['blogPostEditUrl'] ?>">Modifier</a>
-                            <a onclick="sendPostShowOrHide('<?php echo $mapView['id'] ?>')"> <?php if ($mapView['statusP'] != "innactive") {
-                                    echo ($mapView['statusP'] == "active") ? "Mettre en visibilité public" : "Mettre en visibilité privé";
-                                } ?></a>
-                            <!-- De toute facon on affiche pas la publication si innactive + test dans le controller pour la requete post-->
+            <script>
+                function sendPostShowOrHide(id) {
+                    fetch('/Post/BlogEdit/' + id, {
+                        method: 'POST',
+                        body: 'ChangeVisibilityBlog',
+                        headers: {
+                            'Content-Type': 'application/x-www-form-urlencoded'
+                        }
+                    });
+                }
 
-                            <a onclick="sendPostDelete('<?php echo $mapView['id'] ?>')">Supprimer</a>
-                        </div>
-                    </div>
-                    </span>
-                    <p class="lead responsive-text">Catégorie - 00-00-00 - Par l'auteur</p>
-                    <p class="responsive-text">ça va parler chinois : Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit neskwik ça va parler chinois : Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit neskwik ça va parler chinois : Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit neskwik ça va parler chinois : Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit neskwik</p>
-                </div>
-            </div>
+                function sendPostDelete(id) {
+                    fetch('/Post/BlogEdit/' + id, {
+                        method: 'POST',
+                        body: 'DeleteBlog',
+                        headers: {
+                            'Content-Type': 'application/x-www-form-urlencoded'
+                        }
+                    });
+                }
+            </script>
+
         </div>
     </div>
     <script src="../../common_scripts/maxTextSize.js"></script>
