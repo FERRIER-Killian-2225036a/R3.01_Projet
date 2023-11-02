01c0b062fec3df0f9c9fddb9bdc046adc0030883
Killian Ferrier <killian.ferrier@etu.univ-amu.fr>
2023-11-02 01:04:01 +0100

    Test bouton de modif

--- views/profileSettings/myPost.php
+++ views/profileSettings/myPost.php
@@ -54,6 +54,21 @@
                 </div>
             </div>
         </div>
+        <div class="dropdown">
+            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
+                 class="bi bi-three-dots" viewBox="0 0 16 16">
+                <path d="M3 9.5a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3zm5 0a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3zm5 0a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3z"/>
+            </svg>
+            <div class="dropdown-content">
+                <a href="<?php echo $mapView['blogPostEditUrl'] ?>">Modifier</a>
+                <a onclick="sendPostShowOrHide('<?php echo $mapView['id'] ?>')"> <?php if ($mapView['statusP'] != "innactive") {
+                        echo ($mapView['statusP'] == "active") ? "Mettre en visibilité public" : "Mettre en visibilité privé";
+                    } ?></a>
+                <!-- De toute facon on affiche pas la publication si innactive + test dans le controller pour la requete post-->
+
+                <a onclick="sendPostDelete('<?php echo $mapView['id'] ?>')">Supprimer</a>
+            </div>
+        </div>
     </div>
     <script src="../../common_scripts/maxTextSize.js"></script>
 </div>
