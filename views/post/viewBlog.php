e5df5d79e67020daedb4eccdc5f2a8033a381b4a
Killian Ferrier <killian.ferrier@etu.univ-amu.fr>
2023-11-03 15:52:28 +0100

    changement du logo signet

--- views/post/viewBlog.php
+++ views/post/viewBlog.php
@@ -25,8 +25,7 @@
                 <!-- Formulaire pour ajouter aux signets -->
                 <form action="<?php echo $mapView["CurentUrlPost"]?>" method="post">
                     <button name="bookmark" title="Signet" type="submit" id="formSignet">
-                        <!--<img src="../../media/public_assets/icone/signet.png" alt="Signet" class="icon" >-->
-                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-bookmark" id="signetImg" viewBox="0 0 16 16">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="2.3em" height="2.3em" fill="currentColor" class="bi bi-bookmark" id="signetSvg" viewBox="0 0 16 16">
                             <path d="M2 2a2 2 0 0 1 2-2h8a2 2 0 0 1 2 2v13.5a.5.5 0 0 1-.777.416L8 13.101l-5.223 2.815A.5.5 0 0 1 2 15.5V2zm2-1a1 1 0 0 0-1 1v12.566l4.723-2.482a.5.5 0 0 1 .554 0L13 14.566V2a1 1 0 0 0-1-1H4z"/>
                         </svg>
                     </button>
