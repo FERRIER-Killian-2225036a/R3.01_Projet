7febf5916d7a7705e6d3300102e710b15ad5923d
Pierrick Caron <pierrick.caron@etu.univ-amu.fr>
2023-11-03 11:10:54 +0100

    Début de la sideBarModo.php + finition de la sideBar.php

--- views/modo/sideBarModo.php
+++ views/modo/sideBarModo.php
@@ -1,66 +1,51 @@
-<link rel="stylesheet" href="../../common_styles/profileSettings.css">
-<div class="container-fluid ">
+<div class="container-fluid">
     <div class="row" id="container">
         <div class="d-flex flex-column flex-shrink-0 shadow round mt-3 p-2" style="width: 280px;"> <!-- Ajout de la classe 'shadow' ici -->
 
             <!-- Mon compte -->
             <a href="/Settings/" class="align-items-center mb-3 mb-md-0 me-md-auto link-body-emphasis text-decoration-none text-dark"> <!-- Ajout de 'text-dark' ici -->
-                <img src="<?= (SessionManager::isUserConnected() && $_SESSION['UrlPicture']!==null ) ? $_SESSION['UrlPicture'] : Constants::PDP_URL_DEFAULT; ?>" alt="Profil Image" width="40" height="40" class="rounded-circle me-2">
-                <span class="fs-4"><?php echo $_SESSION["Username"]; ?></span>
-                <p class="fs-4" id="mail"><?php echo $_SESSION['Mail'];?></p>
+                <p class="fs-4" id="mail">Modérateur</p>
             </a>
             <hr class="my-3">
             <ul class="nav nav-pills flex-column mb-auto align-items-center">
-                <!-- Information personnel-->
+                <!-- Ticket -->
                 <li class="nav-item">
                     <a href="/Settings/ManageAccount" class="nav-link custom-nav-link text-dark">
-                        Informations personnelles
+                        Ticket
                     </a>
                 </li>
-                <!-- Support -->
+                <!-- En attentes -->
                 <li>
                     <a href="/Settings/Support" class="nav-link custom-nav-link text-dark">
-                        Support
+                        En attentes
                     </a>
                 </li>
-                <!-- Langue -->
+                <!-- En traitement  -->
                 <li>
                     <a href="/Settings/Language" class="nav-link custom-nav-link text-dark">
-                        Langue
+                        En traitement
                     </a>
                 </li>
-                <!-- Thème -->
+                <!-- Fermés -->
                 <li>
                     <a href="/Settings/Theme" class="nav-link custom-nav-link text-dark">
-                        Thème
+                        Fermés
                     </a>
                 </li>
                 <hr class="my-3">
-                <!--Mes enregistrements -->
+
+                <!--Utilisateurs muets -->
                 <li class="nav-item">
                     <a href="/Settings/Bookmark" class="nav-link custom-nav-link text-dark">
-                        Mes enregistrements
+                        Utilisateurs muets
                     </a>
                 </li>
-                <!-- Personnes suivies -->
+                <!-- Blacklist -->
                 <li>
                     <a href="/Settings/Follow" class="nav-link custom-nav-link text-dark">
-                        Personnes suivies
+                        Blacklist
                     </a>
                 </li>
-                <!-- Mes commentaires -->
-                <li>
-                    <a href="/Settings/MyComment" class="nav-link custom-nav-link text-dark">
-                        Mes commentaires
-                    </a>
-                </li>
-                <!-- Mes posts -->
-                <li>
-                    <a href="/Settings/MyPost" class="nav-link custom-nav-link text-dark">
-                        Mes posts
-                    </a>
-                </li>
-            </ul>
             <hr class="hr-color">
             <a href="/Auth/LogOut" class="d-flex align-items-center justify-content-center mb-3 link-body-emphasis text-danger text-decoration-none">
                 <img src="/media/public_assets/icone/iconeSeDeconnecter.png" alt="Logo de déconnexion" class="me-2">
