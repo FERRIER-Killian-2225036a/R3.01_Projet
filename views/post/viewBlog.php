b140d6fb7101d546126738de09033c55121b2f2b
Pierrick Caron <pierrick.caron@etu.univ-amu.fr>
2023-11-03 12:10:28 +0100

    Modification de toute le dossier post avec des commentaires et pour que sa rentre dans la norme W3C

--- views/post/viewBlog.php
+++ views/post/viewBlog.php
@@ -1,23 +1,28 @@
 <link rel="stylesheet" href="/common_styles/post.css">
 <div id="postBlogContainer">
     <div class="text-left mt-4">
+        <!-- Titre du post -->
         <h1><?php echo $mapView['Title'] ?></h1>
         <div class="row mb-4">
+            <!-- Section d'informations sur l'auteur -->
             <div class="col-md-6 d-flex align-items-center">
                 <img src="<?php echo $mapView['ImgProfil'] ?>" alt="image" class="rounded-circle mr-3">
                 <div id="textUser">
                     <p class="mb-0"><?php echo $mapView['Author'] ?></p>
                     <small><?php echo $mapView['NumberOfFollower'] ?> abonnés</small>
                 </div>
+                <!-- Formulaire pour suivre l'auteur -->
                 <form action="<?php echo $mapView["CurentUrlPost"]?>" method="post" >
                     <button class="btn btn-custom-purple" name="follow" type="submit">Suivre</button>
                 </form>
             </div>
+            <!-- Section de partage et signet -->
             <div class="col-md-6 d-flex align-items-center justify-content-end">
                 <label id="copyLabel"></label>
                 <a href="#" title="Partager" id="lienPartager">
                     <img src="../../media/public_assets/icone/partager.png" alt="Partager" class="icon">
                 </a>
+                <!-- Formulaire pour ajouter aux signets -->
                 <form action="<?php echo $mapView["CurentUrlPost"]?>" method="post">
                     <button name="bookmark" title="Signet" type="submit" id="formSignet">
                         <img src="../../media/public_assets/icone/signet.png" alt="Signet" class="icon" id="signetImg">
@@ -26,15 +31,14 @@
             </div>
         </div>
         <hr>
-
         <div class="row justify-content-center mt-4">
             <div class="col-8">
                 <img src="<?php echo $mapView['Img'] ?>" class="img-fluid" alt="Image du Blog" id="imgBlog">
             </div>
         </div>
-
         <div class="row justify-content-center mt-4">
             <div class="col-8">
+                <!-- Contenu du post -->
                 <p style="text-align: justify;">
                     <?php echo $mapView['Content'] ?>
                 </p>
--- views/post/viewBlogEdit.php
+++ views/post/viewBlogEdit.php
@@ -5,46 +5,39 @@
     <div class="p-3">
         <div id="title">
             <h2>Edition nouveau blog</h2>
-            <button class="btn btn-outline-danger"
-                    onclick="window.location.href='https://cyphub.tech/Menu/BlogFeed';">Quitter
-            </button>
+            <button class="btn btn-outline-danger" onclick="window.location.href='https://cyphub.tech/Menu/BlogFeed';">Quitter</button>
         </div>
         <form action="/Post/BlogEdit<?= "/".$mapView["UrlForm"] ?>" method="post" enctype="multipart/form-data">
             <!-- Titre -->
             <label class="bg-body-tertiary round background">
-                <input name="Title" type="text" class="form-control custom-input round inputBackground input"
-                       placeholder="Entrez votre titre" value="<?php echo $mapView["Title"]?>">
+                <input name="Title" type="text" class="form-control custom-input round inputBackground input" placeholder="Entrez votre titre" value="<?php echo $mapView["Title"]?>">
             </label>
 
-            <!--Categorie new -->
+            <!-- Categorie new -->
             <label for="tag-input1">Entrez les tags de votre publication</label>
             <input type="text" name="Tags" id="tag-input1" class="form-control custom-input round inputBackground">
             <label id="wrongInfo"></label>
 
             <!-- Input d'image -->
-            <label for="file"  class="btn btn bg-body-tertiary round background" id="chooseFileLabel">Entrez votre miniature
+            <label for="file" class="btn btn bg-body-tertiary round background" id="chooseFileLabel">Entrez votre miniature
                 <input id="file" type="file" name="BlogPicture" accept=".jpg, .jpeg, .png, .gif" style="display: none;">
-                <img src="<?= ($mapView["Img"]!=null) ? $mapView["Img"] : "../../media/public_assets/icone/iconeBlogUploadImg.png" ?>" width="260" height="300"
-                     alt="logo pour l'upload d'image">
+                <img src="<?= ($mapView["Img"] != null) ? $mapView["Img"] : "../../media/public_assets/icone/iconeBlogUploadImg.png" ?>" width="260" height="300" alt="logo pour l'upload d'image">
             </label>
 
             <!-- Texte -->
             <label class="bg-body-tertiary round background textInput">
-                <textarea name="Content" class="form-control custom-input round inputBackground input"
-                          placeholder="Entrez le contenue" ><?php echo $mapView["Content"]?></textarea>
+                <textarea name="Content" class="form-control custom-input round inputBackground input" placeholder="Entrez le contenu"><?php echo $mapView["Content"]?></textarea>
             </label>
 
             <!-- Bouton publier -->
             <span id="publishButtonContainer">
-                <button class="btn btn-outline" type="submit" id="publishButton" style="resize: none;">Publier</button>
-            </span>
+                    <button class="btn btn-outline" type="submit" id="publishButton" style="resize: none;">Publier</button>
+                </span>
         </form>
     </div>
-    <script src="../../common_scripts/selectFiles.js"></script>
-    <script src="../../common_scripts/simple-tags.js"></script>
-    <script src="/common_scripts/showErrorMessage.js"></script>
-    <script>tagInput1.addData([<?php echo $mapView["Tags"]?>])</script>
-
-
 </main>
 
+<script src="../../common_scripts/selectFiles.js"></script>
+<script src="../../common_scripts/simple-tags.js"></script>
+<script src="/common_scripts/showErrorMessage.js"></script>
+<script>tagInput1.addData([<?php echo $mapView["Tags"]?>])</script>
