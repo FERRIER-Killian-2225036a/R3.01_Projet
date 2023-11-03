03294ba6c0a05f256ba8a3446a1d4bce0a9c7271
Killian Ferrier <killian.ferrier@etu.univ-amu.fr>
2023-11-03 18:00:53 +0100

    debug bouton signet

--- views/menu/blogFeed.php
+++ views/menu/blogFeed.php
@@ -28,7 +28,7 @@
                                     <i class="fas fa-save">#blog</i>
                                 </button>
                             </div>
-                            <form action="<?php echo $mapView["CurentUrlPost"]?>" method="post" class="formSignet">
+                            <form action="<?php echo $mapView["CurentUrlPost"]?>" method="post" class="formSignetClass">
                                 <button name="bookmark" title="Signet" type="submit" id="formSignet">
                                     <svg xmlns="http://www.w3.org/2000/svg" id="svgBookmarkAdd" width="2.3em" height="2.3em" fill="currentColor" class="bi bi-bookmark-plus" viewBox="0 0 16 16">
                                         <path d="M2 2a2 2 0 0 1 2-2h8a2 2 0 0 1 2 2v13.5a.5.5 0 0 1-.777.416L8 13.101l-5.223 2.815A.5.5 0 0 1 2 15.5V2zm2-1a1 1 0 0 0-1 1v12.566l4.723-2.482a.5.5 0 0 1 .554 0L13 14.566V2a1 1 0 0 0-1-1H4z"/>
@@ -69,9 +69,10 @@
         document.getElementById("divButtonRole").addEventListener("click", function() {
             const isInput = event.target.closest('.input-group');
             const isCustomInput = event.target.classList.contains('custom-input');
-            const isformSignet = event.target.closest('.formSignet');
+            const isFormSignetClass = event.target.closest('.formSignetClass');
+            const isFormSignet = event.target.id.contains('formSignet');
 
-            if (!isInput && !isCustomInput && !isformSignet) {
+            if (!isInput && !isCustomInput && !isFormSignetClass) {
                 window.location.href = "<?php echo $mapView['blogPostUrl'] ?>";
             }
         });
