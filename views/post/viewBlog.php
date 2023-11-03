11a975a8036cd6f4a52145d86a9dec1ca1364ef0
Killian Ferrier <killian.ferrier@etu.univ-amu.fr>
2023-11-03 21:30:44 +0100

    ajout de la redirection du signet sur les pages le contenant

--- views/post/viewBlog.php
+++ views/post/viewBlog.php
@@ -148,3 +148,4 @@
         svgBookmarkDel.style.display = 'flex';
     }
 </script>
+<script src="/common_scripts/signetRedirection.js"></script>
--- views/profileSettings/postBookmark.php
+++ views/profileSettings/postBookmark.php
@@ -44,5 +44,6 @@
         svgBookmarkDel.style.display = 'flex';
     }
 </script>
+<script src="/common_scripts/signetRedirection.js"></script>
 </div>
 </div>
