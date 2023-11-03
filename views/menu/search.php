c80cf097a6f15f67ecedf53744e6df679dc4533f
Killian Ferrier <killian.ferrier@etu.univ-amu.fr>
2023-11-03 22:47:54 +0100

    arrangement du style des recherches

--- views/menu/search.php
+++ views/menu/search.php
@@ -1,3 +1,5 @@
+<link rel="stylesheet" href="/common_styles/menu.css">
+<main id="searchContainer">
 <?php
 //array("result" => $result, "input" => $input)
 
@@ -8,24 +10,35 @@ if (isset($mapView["result"]["IT_Articles"])) {
         echo "<a href='" . $url . "'>" . $url . "</a><br>";
     }
 }
+?>
+<hr>
+<?php
 if (isset($mapView["result"]["USERSite"])) {
     echo "<h2>Utilisateurs</h2>";
     foreach ($mapView["result"]["USERSite"] as $url) {
         echo "<a href='" . $url . "'>" . $url . "</a><br>";
     }
 }
+?>
+<hr>
+<?php
 if (isset($mapView["result"]["BLOG_Page"])) {
     echo "<h2>Posts de blog</h2>";
     foreach ($mapView["result"]["BLOG_Page"] as $url) {
         echo "<a href='" . $url . "'>" . $url . "</a><br>";
     }
 }
+?>
+<hr>
+<?php
 if (isset($mapView["result"]["BLOG_Comment"])) {
     echo "<h2>Commentaires</h2>";
     foreach ($mapView["result"]["BLOG_Comment"] as $url) {
         echo "<a href='" . $url . "'>" . $url . "</a><br>";
     }
-}/*
+}
+
+/*
 if (isset($mapView["result"]["FORUM_Page"])) {
     echo "<h2>Posts de forum</h2>";
     foreach ($mapView["result"]["FORUM_Page"] as $url) {
@@ -44,23 +57,31 @@ if (isset($mapView["result"]["FORUM_Category"])) {
         echo "<a href='" . $url . "'>" . $url . "</a><br>";
     }
 }
-*/
+*/?>
+<hr>
+<?php
 if (isset($mapView["result"]["BLOG_Category"])) {
     echo "<h2>blogs avec la catégorie : ". $mapView["input"]." </h2>";
     foreach ($mapView["result"]["BLOG_Category"] as $url) {
         echo "<a href='" . $url . "'>" . $url . "</a><br>";
     }
 }
-
+?>
+<hr>
+<?php
 if (isset($mapView["result"]["BLACKLIST"])) {
     echo "<h2>BlackList</h2>";
     foreach ($mapView["result"]["BLACKLIST"] as $url) {
         echo "<a href='" . $url . "'>" . $url . "</a><br>";
     }
 }
+?>
+<hr>
+<?php
 if (isset($mapView["result"]["MUTE"])) {
     echo "<h2>Muted User</h2>";
     foreach ($mapView["result"]["MUTE"] as $url) {
         echo "<a href='" . $url . "'>" . $url . "</a><br>";
     }
-}
+}?>
+</main>
