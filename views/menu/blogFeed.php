9dab53ddf7af9cd522b6906728054cb9c8afbe99
Killian Ferrier <killian.ferrier@etu.univ-amu.fr>
2023-11-04 18:51:34 +0100

    essaie de resolution de bug

--- views/menu/blogFeed.php
+++ views/menu/blogFeed.php
@@ -71,12 +71,5 @@
             // On affiche le signet de suppression
             svgBookmarkDel.style.display = 'flex';
         }
-
-        document.getElementById("divButtonRole").addEventListener("click", function() {
-            const isInput = event.target.closest('.input-group');
-            if (!isInput) {
-                window.location.href = "<?php echo $mapView['blogPostUrl'] ?>"; //TODO changer php pcq le lien est que sur le premier
-            }
-        });
     </script>
 </main>
