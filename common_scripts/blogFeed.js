b64d2f3f35e71055d8c96dedc5fc4ca1bd1c34a1
Killian Ferrier <killian.ferrier@etu.univ-amu.fr>
2023-11-04 19:18:12 +0100

    essaie de resolution de bug

--- common_scripts/blogFeed.js
+++ common_scripts/blogFeed.js
@@ -18,15 +18,40 @@ divButtonRoles.forEach(function(divButtonRole) {
     });
 });*/
 
-// Sélectionnez tous les éléments avec l'ID "divButtonRole" (supposons qu'ils aient une classe "button-role" pour cette démonstration)
-const divButtonRoles = document.querySelectorAll('.button-role');
 
-// Parcourez chaque élément et ajoutez l'événement
-for(let i=0; i<divButtonRoles.length; i++) {
-    divButtonRoles[i].addEventListener("click", function(event) {
-        const isInput = event.target.closest('.input-group');
-        if (!isInput) {
-            window.location.href = "<?php echo $mapView['blogPostUrl'] ?>";
-        }
-    });
+function redirect() {
+    // Sélectionnez tous les éléments avec l'ID "divButtonRole" (supposons qu'ils aient une classe "button-role" pour cette démonstration)
+    const divButtonRoles = document.querySelectorAll('.button-role');
+
+    // Parcourez chaque élément et ajoutez l'événement
+    for(let i=0; i<divButtonRoles.length; i++) {
+        divButtonRoles[i].addEventListener("click", function(event) {
+            const isInput = event.target.closest('.input-group');
+            if (!isInput) {
+                window.location.href = "<?php echo $mapView['blogPostUrl'] ?>";
+            }
+        });
+    }
+}
+
+
+function bookmark() {
+    try {
+        let boolIsPostBookmarked = 0;
+        let svgBookmarkAdd;
+        let svgBookmarkDel;
+    } catch {}
+    // Récupération du booleen pour savoir si le post est enregistré ou pas
+    boolIsPostBookmarked = "<?php echo $mapView['BoolIsPostBookmarked'] ? 1 : 0 ?>";
+    // Sélection du signet d'ajout
+    svgBookmarkAdd = document.getElementById('svgBookmarkAdd');
+    // Sélection du signet de suppression
+    svgBookmarkDel = document.getElementById('svgBookmarkDel');
+    // Condition si l'utilisateur a enregistré le post
+    if (boolIsPostBookmarked === 1) {
+        // On cache le signet d'ajout
+        svgBookmarkAdd.style.display = 'none';
+        // On affiche le signet de suppression
+        svgBookmarkDel.style.display = 'flex';
+    }
 }
