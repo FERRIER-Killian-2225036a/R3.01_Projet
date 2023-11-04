b344631705b0850db6f2fd5087719ef3de4f0e88
Killian Ferrier <killian.ferrier@etu.univ-amu.fr>
2023-11-04 20:36:24 +0100

    Ajout des commentaires

--- common_scripts/blogFeed.js
+++ common_scripts/blogFeed.js
@@ -1,23 +1,33 @@
-function redirect(stringOfLikns) {
-    // Sélectionnez tous les éléments avec l'ID "divButtonRole" (supposons qu'ils aient une classe "button-role" pour cette démonstration)
+// Fonction pour la redirection quand on clique sur un blog dans le blogFeed avec l'exception pour l'input
+function redirect(stringOfPostId) {
+    // Sélection de tous les éléments avec l'ID "divButtonRole"
     const divButtonRoles = document.querySelectorAll('.button-role');
-    const listOfLikns = stringOfLikns.split(',');
-    // Parcourez chaque élément et ajoutez l'événement
+    // Séparation des éléments de stringOfPostId au niveau des virgules pour les entrer dans un tableau
+    const listOfPostId = stringOfPostId.split(',');
+    // Boucle parcourant tous les éléments de divButtonRoles
     for(let i=0; i<divButtonRoles.length; i++) {
+        // Ajout d'un EventListener pour l'action du clique sur un élément de divButtonRoles
         divButtonRoles[i].addEventListener("click", function(event) {
+            // Récupération de l'élément dont on ne veut pas que l'event listener est effet
             const isInput = event.target.closest('.input-group');
+            // Si on clique ailleurs que le bouton dont on ne veut pas:
             if (!isInput) {
-                window.location.href = "https://cyphub.tech/Post/Blog/" + listOfLikns[i];
+                // Redirection vers la page correspondent au blog cliqué
+                window.location.href = "https://cyphub.tech/Post/Blog/" + listOfPostId[i];
             }
         });
     }
 }
 
-
-function bookmark(listOfBooleen) {
+// Fonction pour chnager l'icone du signet d'enregistrement d'article
+function bookmark(listOfBooleen) { // listOfBooleen est un tableau avec '1' si l'utilisateur a enregistré l'article et '0' pour l'inverse
+    // Récupération du svg d'enregistrement
     const svgBookmarkAdd = document.querySelectorAll('#svgBookmarkAdd');
+    // Récupération du svg de suppression
     const svgBookmarkDel = document.querySelectorAll('#svgBookmarkDel');
+    // Boucle parcourant toute la string listOfBooleen
     for (let i = 0; i < listOfBooleen.length; i++) {
+        // Condition si l'article est enregistré
         if (listOfBooleen[i] === '1') {
             // On cache le signet d'ajout
             svgBookmarkAdd[i].style.display = 'none';
