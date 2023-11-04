fcdf85f392904564686943ff737846919c2e40e4
Killian Ferrier <killian.ferrier@etu.univ-amu.fr>
2023-11-04 19:02:54 +0100

    essaie de resolution de bug

--- common_scripts/blogFeed.js
+++ common_scripts/blogFeed.js
@@ -1,7 +1,19 @@
-document.getElementById("divButtonRole").addEventListener("click", function() {
+/*document.getElementById("divButtonRole").addEventListener("click", function() {
     const isInput = event.target.closest('.input-group');
     if (!isInput) {
         window.location.href = "<?php echo $mapView['blogPostUrl'] ?>"; //TODO changer php pcq le lien est que sur le premier
     }
-});
+});*/
 
+// Sélectionnez tous les éléments avec l'ID "divButtonRole" (supposons qu'ils aient une classe "button-role" pour cette démonstration)
+const divButtonRoles = document.querySelectorAll('.button-role');
+
+// Parcourez chaque élément et ajoutez l'événement
+divButtonRoles.forEach(function(divButtonRole) {
+    divButtonRole.addEventListener("click", function(event) {
+        const isInput = event.target.closest('.input-group');
+        if (!isInput) {
+            window.location.href = "<?php echo $mapView['blogPostUrl'] ?>";
+        }
+    });
+});
