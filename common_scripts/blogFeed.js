9589450f7316b7eff6c4b90828e8523b0a2602ee
Killian Ferrier <killian.ferrier@etu.univ-amu.fr>
2023-11-04 20:23:51 +0100

    mise au propre du code

--- common_scripts/blogFeed.js
+++ common_scripts/blogFeed.js
@@ -15,25 +15,6 @@ function redirect(stringOfLikns) {
 
 
 function bookmark(listOfBooleen) {
-    /*try {
-        let boolIsPostBookmarked = 0;
-        let svgBookmarkAdd;
-        let svgBookmarkDel;
-    } catch {}
-    // Récupération du booleen pour savoir si le post est enregistré ou pas
-    boolIsPostBookmarked = "<?php echo $mapView['BoolIsPostBookmarked'] ? 1 : 0 ?>";
-    // Sélection du signet d'ajout
-    svgBookmarkAdd = document.getElementById('svgBookmarkAdd');
-    // Sélection du signet de suppression
-    svgBookmarkDel = document.getElementById('svgBookmarkDel');
-    // Condition si l'utilisateur a enregistré le post
-    if (boolIsPostBookmarked === 1) {
-        // On cache le signet d'ajout
-        svgBookmarkAdd.style.display = 'none';
-        // On affiche le signet de suppression
-        svgBookmarkDel.style.display = 'flex';
-    }*/
-    console.log(listOfBooleen);
     const svgBookmarkAdd = document.querySelectorAll('#svgBookmarkAdd');
     const svgBookmarkDel = document.querySelectorAll('#svgBookmarkDel');
     for (let i = 0; i < listOfBooleen.length; i++) {
