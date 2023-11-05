51dce3fe329277aacba3e6adfc109e2ad9a9c9df
Killian Ferrier <killian.ferrier@etu.univ-amu.fr>
2023-11-05 15:15:41 +0100

    debug php

--- controllers/ControllerUser.php
+++ controllers/ControllerUser.php
@@ -87,10 +87,27 @@ class ControllerUser
                             <script>
                                 console.log('<?php echo $arrayOfPageId; ?>')
                                 redirect('<?php echo $arrayOfPageId; ?>');
+                                // Script pour le changement du bouton suivre
+                                // Sélection du bouton suivre
+                                const followedButton = document.getElementsByClassName('followButton');
+                                // Récupération du boolen pour savoir si on suit l'utilisateur qui a posté
+                                let boolIsFollowed = <?php echo $mapView['BoolIsFollowed'] ? 1 : 0 ?>; // TODO php mauvais user, a changer (author au lieu de user) pas sure a verif ?
+                                // Condition si l'utilisateur est suivi
+                                if (boolIsFollowed === 1) {
+                                    for (let i = 0; i < followedButton.length; ++i) {
+                                        // On change le texte du bouton
+                                        followedButton[i].innerHTML = 'Suivi';
+                                        // On met le fond en violet
+                                        followedButton[i].style.backgroundColor = 'var(--purple)';
+                                        // On met le texte en blanc
+                                        followedButton[i].style.color = 'white';
+                                    }
+                                }
                             </script>
                             <?php
                             echo '</div></div>';
                             echo '<script src="/common_scripts/myPostDisplay.js"></script>';
+                            echo '<script src="/common_scripts/maxTextSize.js"></script>';
 
                         }
 
