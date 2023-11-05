51950317015a21c5d3567580ab8501762838449f
Killian Ferrier <killian.ferrier@etu.univ-amu.fr>
2023-11-05 16:27:24 +0100

    resolution redirection au suivi

--- common_scripts/isFollowed.js
+++ common_scripts/isFollowed.js
@@ -1,27 +1,9 @@
 // Script pour le changement du bouton suivre
 function isFollowed(listOfBooleen) {
-    // Sélection du bouton suivre
-    /*const followedButton = document.getElementsByClassName('followButton');
-    // Récupération du boolen pour savoir si on suit l'utilisateur qui a posté
-    let boolIsFollowed = <?php echo $mapView['BoolIsFollowed'] ? 1 : 0 ?>; // TODO php mauvais user, a changer (author au lieu de user) pas sure a verif ?
-    // Condition si l'utilisateur est suivi
-    if (boolIsFollowed === 1) {
-        for (let i = 0; i < followedButton.length; ++i) {
-            // On change le texte du bouton
-            followedButton[i].innerHTML = 'Suivi';
-            // On met le fond en violet
-            followedButton[i].style.backgroundColor = 'var(--purple)';
-            // On met le texte en blanc
-            followedButton[i].style.color = 'white';
-        }
-    }*/
-    console.log(listOfBooleen);
     // Sélection des boutons suivre
     const followedButton = document.querySelectorAll('.followButton');
-    console.log(followedButton);
     for (let i = 0; i < listOfBooleen.length; i++) {
         // Condition si l'utilisateur est suivi
-        console.log(listOfBooleen[i])
         if (listOfBooleen[i] === '1') {
             // On change le texte du bouton
             followedButton[i+1].innerHTML = 'Suivi';
