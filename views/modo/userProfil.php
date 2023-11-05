49249595a18838d6070811cfbb76c09817bbf710
Pierrick Caron <pierrick.caron@etu.univ-amu.fr>
2023-11-05 12:12:29 +0100

    Début de la page userProfil.php

--- views/modo/userProfil.php
+++ views/modo/userProfil.php
@@ -1,1 +1,23 @@
-<?php require 'sideBarModo.php' ?>
+<div class="container">
+    <div class="d-flex align-items-center">
+        <img src="../../media/public_assets/ppTest.jpg" alt="Profil Image" width="40" height="40" class="rounded-circle me-2">
+        <div>
+            <span class="fs-4">krokmou</span>
+            <div class="d-flex">
+                <span class="me-3">99 Abonnés</span>
+                <span class="me-3">99 Abonnements</span>
+                <span class="me-3">1K Posts</span>
+            </div>
+            <button class="btn btn-outline-dark">S'abonner</button>
+        </div>
+    </div>
+
+    <hr>
+
+    <h3>Action modérateur</h3>
+    <ul>
+        <button class="btn">Voir les posts de l'utilisateur</button>
+        <button class="btn">Rendre l'utilisateur muet</button>
+        <button class="btn">Blacklister l'utilisateur</button>
+    </ul>
+</div>
