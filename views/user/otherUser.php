35f81fb7511e90bb325a5694c88b02ab093adac7
Killian Ferrier <killian.ferrier@etu.univ-amu.fr>
2023-11-05 15:26:16 +0100

    réparation du bouton filtrer

--- views/user/otherUser.php
+++ views/user/otherUser.php
@@ -17,18 +17,18 @@
                 </form>
             </div>
         </div>
-        <!-- Colonne de droite TODO a reparer -->
+        <!-- Section pour le dropdown filtrer -->
         <div class="col-md-6 d-flex align-items-center justify-content-end">
             <div class="dropdown">
-                <button type="button" class="btn btn-primary">
-                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-filter" viewBox="0 0 16 16">
+                <button type="button" class="btn btn-primary" id="filterButton">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
+                         class="bi bi-filter" viewBox="0 0 16 16">
                         <path d="M6 10.5a.5.5 0 0 1 .5-.5h3a.5.5 0 0 1 0 1h-3a.5.5 0 0 1-.5-.5zm-2-3a.5.5 0 0 1 .5-.5h7a.5.5 0 0 1 0 1h-7a.5.5 0 0 1-.5-.5zm-2-3a.5.5 0 0 1 .5-.5h11a.5.5 0 0 1 0 1h-11a.5.5 0 0 1-.5-.5z"></path>
                     </svg>
                     Filtrer
                 </button>
                 <ul class="dropdown-menu">
-                    <label for="myInput">
-                    <input class="form-control" id="myInput" type="text" placeholder="Search.."></label>
+                    <label><input class="form-control" id="myInput" type="text" placeholder="Search.."></label>
                     <li><a href="#">Data</a></li>
                     <li><a href="#">Cybersec</a></li>
                     <li><a href="#">IDK</a></li>
