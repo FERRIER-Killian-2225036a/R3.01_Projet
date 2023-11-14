d561e5351cdfc079ea72448decce24968c8eb651
Pierrick Caron <pierrick.caron@etu.univ-amu.fr>
2023-11-14 11:20:34 +0100

    Update

---
+++ views/menu/forumFeed.php
@@ -0,0 +1,28 @@
+<link rel="stylesheet" href="../../common_styles/general.css">
+<main class="container p-5">
+    <div class="p-3">
+        <div class="btn bg-body-tertiary round background grow-button" role="button">
+            <img src="../../media/public_assets/imageTest.jpeg" alt="Forum Icon" class="responsive-image round p-1">
+            <div class="text-content">
+                <h1 class="responsiveTitle">Discussing Cyber Security</h1>
+                <p class="lead"> By Alex_Watson - 15 posts - Last updated: 12 May 2023 </p>
+                <p class="responsive-text">Join our discussion on the latest trends and threats in the cybersecurity world. Share your insights, ask questions, and collaborate with professionals. Remember to keep the conversation respectful and relevant.</p>
+
+                <div class="col-12 col-md-6 col-lg-4">
+                    <div class="input-group align-items-center d-flex">
+                        <label class="flex-grow-1">
+                            <input type="text" class="form-control custom-input inputBackground" placeholder="Répondre">
+                        </label>
+                        <div class="input-group-append">
+                            <a href="bookmark">
+                                <img src="../../media/public_assets/icone/signet.png" alt="Bookmark Icon" id="signetImg">
+                            </a>
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+        </div>
+    </div>
+    <script src="../../common_scripts/maxTextSize.js"></script>
+</main>
--- views/modo/ticket.php
+++ views/modo/ticket.php
@@ -1,1 +1,28 @@
 <?php require 'sideBarModo.php' ?>
+<div class="col container" id="rightSide">
+    <!--Pas sur que sa fonctionne à voir avec Killian et Tom -->
+
+    <!-- Section pour le titre -->
+    <div class="row mb-2 mt-2">
+        <div class="col-md-6">
+            <h1>Mes enregistrements </h1>
+        </div>
+        <!-- Section pour le dropdown filtrer -->
+        <div class="col-md-6 d-flex align-items-center justify-content-end">
+            <div class="dropdown">
+                <button type="button" class="btn btn-primary">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-filter" viewBox="0 0 16 16">
+                        <path d="M6 10.5a.5.5 0 0 1 .5-.5h3a.5.5 0 0 1 0 1h-3a.5.5 0 0 1-.5-.5zm-2-3a.5.5 0 0 1 .5-.5h7a.5.5 0 0 1 0 1h-7a.5.5 0 0 1-.5-.5zm-2-3a.5.5 0 0 1 .5-.5h11a.5.5 0 0 1 0 1h-11a.5.5 0 0 1-.5-.5z"></path>
+                    </svg>
+                    Filtrer
+                </button>
+                <ul class="dropdown-menu">
+                    <label for="myInput">
+                        <input class="form-control" id="myInput" type="text" placeholder="Search..">
+                    </label>
+                    <li><a href="#">Data</a></li>
+                    <li><a href="#">Cybersec</a></li>
+                    <li><a href="#">IDK</a></li>
+                </ul>
+            </div>
+        </div>
